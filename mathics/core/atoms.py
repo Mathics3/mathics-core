fb47bb2f35500a06b9c3c80e0120701e227de912
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-09-27 14:13:45 -0400

    Improve from_mpmath performance

--- mathics/core/atoms.py
+++ mathics/core/atoms.py
@@ -36,18 +36,20 @@ COMPARE_PREC = 50
 @lru_cache(maxsize=1024)
 def from_mpmath(value, prec=None):
     "Converts mpf or mpc to Number."
+    if isinstance(value, mpmath.mpc):
+        if value.imag == 0.0:
+            value = value.real
+        else:
+            real = from_mpmath(value.real, prec)
+            imag = from_mpmath(value.imag, prec)
+            return Complex(real, imag)
+
     if isinstance(value, mpmath.mpf):
         if prec is None:
             return MachineReal(float(value))
         else:
             # HACK: use str here to prevent loss of precision
             return PrecisionReal(sympy.Float(str(value), prec))
-    elif isinstance(value, mpmath.mpc):
-        if value.imag == 0.0:
-            return from_mpmath(value.real, prec)
-        real = from_mpmath(value.real, prec)
-        imag = from_mpmath(value.imag, prec)
-        return Complex(real, imag)
     else:
         raise TypeError(type(value))
 
