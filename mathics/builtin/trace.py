7ba4f895af768eb6362c3a21089cbdedf5c0acad
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-10-06 11:46:09 -0400

    Add the option SortBy to TracedBuiltins

--- mathics/builtin/trace.py
+++ mathics/builtin/trace.py
@@ -48,30 +48,67 @@ class TraceBuiltins(Builtin):
      : count msecs  Builtin name
      : ...
      = -Graphics3D-
+
+    The default is sorting the builtin names by calls count.
+    >> TraceBuiltins[Times[x, x], SortBy->"count"]
+     : count msecs  Builtin name
+     : ...
+     = x^2
+
+    But you can also sort by name, or time.
+
+    The default is sorting the builtin names by type.
+    >> TraceBuiltins[Plus @@ {1, x, x x}, SortBy->"name"]
+     : count msecs  Builtin name
+     : ...
+     = 1 + x + x^2
     """
 
-    custom_evaluation: Evaluation = None
+    options = {
+        "SortBy": "count",
+    }
 
-    def dump_tracing_stats(self):
+    messages = {
+        "wsort": '`1` must be one of the following: "count", "name", "time"',
+    }
+
+    traced_evaluation: Evaluation = None
+
+    def dump_tracing_stats(self, sort_by: str, evaluation):
+        if sort_by not in ("count", "name", "time"):
+            sort_by = "count"
+            evaluation.message("TraceBuiltins", "wsort", sort_by)
+            print()
+
         print("count msecs  Builtin name")
+
+        if sort_by == "count":
+            inverse = True
+            sort_fn = lambda tup: tup[1]["count"]
+        elif sort_by == "time":
+            inverse = True
+            sort_fn = lambda tup: tup[1]["elapsed_milliseconds"]
+        else:
+            inverse = False
+            sort_fn = lambda tup: tup[0]
 
         for name, statistic in sorted(
             function_stats.items(),
-            key=lambda tup: tup[1]["count"],
-            reverse=True,
+            key=sort_fn,
+            reverse=inverse,
         ):
             print(
                 "%5d %6g %s"
                 % (statistic["count"], int(statistic["elapsed_milliseconds"]), name)
             )
 
-    def apply(self, expr, evaluation):
-        "TraceBuiltins[expr_]"
+    def apply(self, expr, evaluation, options={}):
+        "%(name)s[expr_, OptionsPattern[%(name)s]]"
 
         # Reset function_stats
         function_stats = defaultdict(lambda: {"count": 0, "elapsed_milliseconds": 0.0})
 
-        if TraceBuiltins.custom_evaluation is None:
+        if TraceBuiltins.traced_evaluation is None:
             do_replace_copy = BuiltinRule.do_replace
 
             # Replaces do_replace by the custom one
@@ -79,15 +116,18 @@ class TraceBuiltins(Builtin):
 
             # Create new definitions uses the new do_replace
             definitions = Definitions(add_builtin=True)
-            TraceBuiltins.custom_evaluation = Evaluation(definitions=definitions)
+            TraceBuiltins.traced_evaluation = Evaluation(definitions=definitions)
 
-            result = expr.evaluate(TraceBuiltins.custom_evaluation)
+            result = expr.evaluate(TraceBuiltins.traced_evaluation)
 
             # Reverts do_replace to what it was
             BuiltinRule.do_replace = do_replace_copy
         else:
-            result = expr.evaluate(TraceBuiltins.custom_evaluation)
+            result = expr.evaluate(TraceBuiltins.traced_evaluation)
 
-        self.dump_tracing_stats()
+        self.dump_tracing_stats(
+            sort_by=self.get_option(options, "SortBy", evaluation).get_string_value(),
+            evaluation=evaluation,
+        )
 
         return result
