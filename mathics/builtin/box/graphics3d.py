Mathics3/mathics-core

commit 0575f3d8eb574daf12eadf60b0786ea60839eb3c

Clear `Graphics3DBox`

mathics/builtin/box/graphics3d.py
@@ -98,14 +98,12 @@ class Graphics3DBox(GraphicsBox):
                     "position": [0, 2, 2],
                 },
             ]
-        elif lighting == "System`None":
-            pass
 
         elif isinstance(lighting, list) and all(
             isinstance(light, list) for light in lighting
         ):
             for light in lighting:
-                if light[0] in ['"Ambient"', '"Directional"', '"Point"', '"Spot"']:
+                if light[0] in ('"Ambient"', '"Directional"', '"Point"', '"Spot"'):
                     try:
                         head = light[1].get_head_name()
                     except AttributeError:
@@ -211,8 +209,6 @@ class Graphics3DBox(GraphicsBox):
         boxratios = self.graphics_options["System`BoxRatios"].to_python()
         if boxratios == "System`Automatic":
             boxratios = ["System`Automatic"] * 3
-        else:
-            boxratios = boxratios
         if not isinstance(boxratios, list) or len(boxratios) != 3:
             raise BoxConstructError
 
@@ -239,7 +235,7 @@ class Graphics3DBox(GraphicsBox):
                         xmin -= 1
                         xmax += 1
                 elif isinstance(plot_range[0], list) and len(plot_range[0]) == 2:
-                    xmin, xmax = list(map(float, plot_range[0]))
+                    xmin, xmax = float(plot_range[0][0]), float(plot_range[0][1])
                     xmin = elements.translate((xmin, 0, 0))[0]
                     xmax = elements.translate((xmax, 0, 0))[0]
                 else:
@@ -253,7 +249,7 @@ class Graphics3DBox(GraphicsBox):
                         ymin -= 1
                         ymax += 1
                 elif isinstance(plot_range[1], list) and len(plot_range[1]) == 2:
-                    ymin, ymax = list(map(float, plot_range[1]))
+                    ymin, ymax = float(plot_range[1][0]), float(plot_range[1][1])
                     ymin = elements.translate((0, ymin, 0))[1]
                     ymax = elements.translate((0, ymax, 0))[1]
                 else:
@@ -267,7 +263,7 @@ class Graphics3DBox(GraphicsBox):
                         zmin -= 1
                         zmax += 1
                 elif isinstance(plot_range[1], list) and len(plot_range[1]) == 2:
-                    zmin, zmax = list(map(float, plot_range[2]))
+                    zmin, zmax = float(plot_range[2][0]), float(plot_range[2][1])
                     zmin = elements.translate((0, 0, zmin))[2]
                     zmax = elements.translate((0, 0, zmax))[2]
                 else:
@@ -277,11 +273,11 @@ class Graphics3DBox(GraphicsBox):
 
             boxscale = [1.0, 1.0, 1.0]
             if boxratios[0] != "System`Automatic":
-                boxscale[0] = boxratios[0] / (xmax - xmin)
+                boxscale[0] /= xmax - xmin
             if boxratios[1] != "System`Automatic":
-                boxscale[1] = boxratios[1] / (ymax - ymin)
+                boxscale[1] /= ymax - ymin
             if boxratios[2] != "System`Automatic":
-                boxscale[2] = boxratios[2] / (zmax - zmin)
+                boxscale[2] /= zmax - zmin
 
             if final_pass:
                 xmin *= boxscale[0]
@@ -302,14 +298,17 @@ class Graphics3DBox(GraphicsBox):
                             light["target"][j] * boxscale[j] for j in range(3)
                         ]
 
-            w = 0 if (xmin is None or xmax is None) else xmax - xmin
-            h = 0 if (ymin is None or ymax is None) else ymax - ymin
+            return xmin, xmax, ymin, ymax, zmin, zmax, boxscale
 
-            return xmin, xmax, ymin, ymax, zmin, zmax, boxscale, w, h
-
-        xmin, xmax, ymin, ymax, zmin, zmax, boxscale, w, h = calc_dimensions(
-            final_pass=False
-        )
+        (
+            xmin,
+            xmax,
+            ymin,
+            ymax,
+            zmin,
+            zmax,
+            boxscale,
+        ) = calc_dimensions(final_pass=False)
 
         axes, ticks, ticks_style = self.create_axes(
             elements,
@@ -356,8 +355,7 @@ class Graphics3DBox(GraphicsBox):
 
         elements._apply_boxscaling(boxscale)
 
-        xmin, xmax, ymin, ymax, zmin, zmax, boxscale, w, h = calc_dimensions()
-        elements.view_width = w
+        xmin, xmax, ymin, ymax, zmin, zmax, boxscale = calc_dimensions()
 
         # FIXME: json is the only thing we can convert MathML into.
         # Handle other graphics formats.
@@ -415,7 +413,7 @@ class Graphics3DBox(GraphicsBox):
         else:
             asy = elements.to_asy()
 
-        xmin, xmax, ymin, ymax, zmin, zmax, boxscale, w, h = calc_dimensions()
+        xmin, xmax, ymin, ymax, zmin, zmax, boxscale = calc_dimensions()
 
         # TODO: Intelligently place the axes on the longest non-middle edge.
         # See algorithm used by web graphics in mathics/web/media/graphics.js

mathics/builtin/drawing/graphics3d.py
@@ -62,6 +62,7 @@ class Style3D(Style):
     def get_default_face_color(self):
         return RGBColor(components=(1, 1, 1, 1))
 
+
 class Graphics3D(Graphics):
     r"""
     <dl>

commit fd85b4a462252dfde14c2083e56a4f1f0b8ac34b

Fix the distortion in Plot3D The bug was introduced in 0575f3d8eb574daf12eadf60b0786ea60839eb3c (thanks @rocky for finding it). Plot3D was always drawing cube plots, even if the height was between -0.5 and 0.5 (supposing the range is -1 and 1), it was "distorted" to -1 and 1.

mathics/builtin/box/graphics3d.py
@@ -273,11 +273,11 @@ class Graphics3DBox(GraphicsBox):
 
             boxscale = [1.0, 1.0, 1.0]
             if boxratios[0] != "System`Automatic":
-                boxscale[0] /= xmax - xmin
+                boxscale[0] = boxratios[0] / (xmax - xmin)
             if boxratios[1] != "System`Automatic":
-                boxscale[1] /= ymax - ymin
+                boxscale[1] = boxratios[1] / (ymax - ymin)
             if boxratios[2] != "System`Automatic":
-                boxscale[2] /= zmax - zmin
+                boxscale[2] = boxratios[2] / (zmax - zmin)
 
             if final_pass:
                 xmin *= boxscale[0]