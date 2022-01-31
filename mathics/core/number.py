802ef99169a67ceeddd2fff1adecdf343afb9f12
mmatera <matera@fisica.unlp.edu.ar>
2022-01-31 18:27:57 -0300

    merge_step1

--- mathics/core/number.py
+++ mathics/core/number.py
@@ -56,7 +56,7 @@ def _get_float_inf(value, evaluation) -> typing.Optional[float]:
     return value.round_to_float(evaluation)
 
 
-def get_precision(value, evaluation) -> typing.Optional[int]:
+def get_precision(value, evaluation, show_messages=True) -> typing.Optional[int]:
     if value is SymbolMachinePrecision:
         return None
     else:
@@ -67,14 +67,17 @@ def get_precision(value, evaluation) -> typing.Optional[int]:
         d = value.round_to_float(evaluation)
         assert dmin is not None and dmax is not None
         if d is None:
-            evaluation.message("N", "precbd", value)
+            if show_messages:
+                evaluation.message("N", "precbd", value)
         elif d < dmin:
             dmin = int(dmin)
-            evaluation.message("N", "precsm", value, MachineReal(dmin))
+            if show_messages:
+                evaluation.message("N", "precsm", value, MachineReal(dmin))
             return dmin
         elif d > dmax:
             dmax = int(dmax)
-            evaluation.message("N", "preclg", value, MachineReal(dmax))
+            if show_messages:
+                evaluation.message("N", "preclg", value, MachineReal(dmax))
             return dmax
         else:
             return d
