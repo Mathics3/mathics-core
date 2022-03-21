b2874de3c48673db1ae8811ad1510d568a4d33d3
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-03-21 17:09:37 +0000

    Improve Factor documentation - @rocky comment

--- mathics/builtin/numbers/algebra.py
+++ mathics/builtin/numbers/algebra.py
@@ -576,9 +576,21 @@ class Factor(Builtin):
     >> Factor[1 / (x^2+2x+1) + 1 / (x^4+2x^2+1)]
      = (2 + 2 x + 3 x ^ 2 + x ^ 4) / ((1 + x) ^ 2 (1 + x ^ 2) ^ 2)
 
-    ## Issue659
-    #> Factor[{x+x^2}]
-     = {x (1 + x)}
+    Factor can also be used with equations:
+    >> Factor[x a == x b + x c]
+     = a x == x (b + c)
+
+    And lists:
+    >> Factor[{x + x^2, 2 x + 2 y + 2}]
+     = {x (1 + x), 2 (1 + x + y)}
+
+    It also works with more complex expressions:
+    >> Factor[x ^ 3 + 3 x ^ 2 y + 3 x y ^ 2 + y ^ 3]
+     = (x + y) ^ 3
+
+    You can use Factor to find when a polynomial is zero:
+    >> x^2 - x == 0 // Factor
+     = x (-1 + x) == 0
     """
 
     attributes = listable | protected
