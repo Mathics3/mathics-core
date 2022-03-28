5c32053efa9f8f1ac57233936b59ceb9f767849e
Hannes Rüter <git@hannesrueter.de>
2022-03-28 19:58:33 +0100

    Handle expressions not representable by SymPy

--- mathics/builtin/numbers/algebra.py
+++ mathics/builtin/numbers/algebra.py
@@ -418,8 +418,9 @@ class Apart(Builtin):
 
         expr_sympy = expr.to_sympy()
         var_sympy = var.to_sympy()
+        # If the expression cannot be handled by Sympy, just return it.
         if expr_sympy is None or var_sympy is None:
-            return None
+            return expr
 
         try:
             result = sympy.apart(expr_sympy, var_sympy)
@@ -1392,8 +1393,9 @@ class Factor(Builtin):
         "Factor[expr_]"
 
         expr_sympy = expr.to_sympy()
+        # If the expression cannot be handled by Sympy, just return it.
         if expr_sympy is None:
-            return None
+            return expr
 
         try:
             return from_sympy(sympy_factor(expr_sympy))
