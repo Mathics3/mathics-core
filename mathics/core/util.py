81824e33deffa95f6d145bff846c4abfc71d6040
rocky <rb@dustyfeet.com>
2022-05-30 19:49:34 -0400

    Add TimingContextManager()
    this can be used as a context manager to time specific parts of the code.

--- mathics/core/util.py
+++ mathics/core/util.py
@@ -42,6 +42,29 @@ def timeit(method):
     return timed
 
 
+class TimeitContextManager:
+    """Add this as a context manager to time parts of the code.
+
+    For example:
+        with TimeitContextManger("testing my loop"):
+           for x in collection:
+               ...
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __enter__(self):
+        # print(f"{date.today()}	{method_name} starts")
+        self.ts = time.time()
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        te = time.time()
+        elapsed = int((te - self.ts) * 1000)
+        if elapsed > MIN_ELAPSE_REPORT:
+            print("%r  %2.2f ms" % (self.name, elapsed))
+
+
 def interpolate_string(text, get_param) -> str:
     index = [1]
 
