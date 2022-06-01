197170bf563334c674b8c3a2ff8b4b050a253901
rocky <rb@dustyfeet.com>
2022-06-01 19:23:45 -0400

    Revise for new ListExpresion(), Expression()...
    to_expression, to_mathics_list

--- test/core/test_arithmetic.py
+++ test/core/test_arithmetic.py
@@ -5,7 +5,6 @@
 import unittest
 from mathics.core.atoms import (
     Integer,
-    Integer0,
     Integer1,
     Integer2,
     Integer3,
@@ -14,8 +13,8 @@ from mathics.core.atoms import (
 )
 from mathics.core.definitions import Definitions
 from mathics.core.evaluation import Evaluation
-from mathics.core.expression import Expression
-from mathics.core.list import ListExpression
+from mathics.core.expression import Expression, to_expression
+from mathics.core.list import ListExpression, to_mathics_list
 from mathics.core.symbols import Symbol, SymbolAbs, SymbolPlus, SymbolTimes
 
 
@@ -23,52 +22,50 @@ class ArithmeticTest(unittest.TestCase):
     def setUp(self):
         definitions = Definitions(add_builtin=True)
         self.evaluation = Evaluation(definitions, format="xml")
+        self.symbol_a = Symbol("a")
 
-    def testAdd(self):
+    def testSubtraction(self):
+        """
+        Test forms of binary infix operator subtraction
+        """
         cases = (
-            (Symbol("a") + Integer3, Expression(SymbolPlus, Integer3, Symbol("a"))),
-            (Integer1 + Integer3, Integer(4)),
-            (Integer1 + -Integer3, -Integer2),
-            (
-                ListExpression(Integer1, Integer2)
-                + ListExpression(IntegerM1, Integer(8)),
-                ListExpression(Integer0, Integer(10)),
-            ),
-        )
-        self._testCases(cases)
-
-    def testSub(self):
-        cases = (
-            (Symbol("a") - 3, Expression(SymbolPlus, -3, Symbol("a"))),
+            (self.symbol_a - 3, Expression(SymbolPlus, -3, self.symbol_a)),
             (Integer1 - 3, Integer(-2)),
             (Integer1 - (-3), Integer(4)),
             (
-                ListExpression(Integer1, Integer2)
-                - ListExpression(IntegerM1, Integer(8)),
-                ListExpression(Integer2, -Integer(6)),
+                # test using to_mathics_list() and with element_convesion_fn
+                to_mathics_list(1, 2) - to_mathics_list(-1, 8),
+                to_mathics_list(2, -6, elements_conversion_fn=Integer),
             ),
         )
         self._testCases(cases)
 
-    def testMul(self):
+    def testMultiplication(self):
+        """
+        Test forms of binary infix operator multiplication
+        """
         cases = (
-            (Symbol("a") * 3, Expression("Times", 3, Symbol("a"))),
+            (self.symbol_a * 3, Expression(SymbolTimes, 3, self.symbol_a)),
             (Integer(3) * 7, Integer(21)),
-            (Integer(3) * (-7), Integer(-21)),
+            (Integer(3) * -7, Integer(-21)),
             (
-                Expression("List", 1, 2) * Expression("List", -1, 8),
-                Expression("List", -1, 16),
+                to_mathics_list(1, 2) * to_mathics_list(-1, 8),
+                to_mathics_list(-1, 16, elements_conversion_fn=Integer),
             ),
         )
         self._testCases(cases)
 
-    def testTrueDiv(self):
+    def testTrueDivision(self):
+        """
+        Test forms of binary infix operator true division
+        """
         cases = (
             (Symbol("a") / 3, Expression(SymbolTimes, Rational(1, 3), Symbol("a"))),
             (Integer(8) / 2, Integer(4)),
-            (Integer(8) / (-2), Integer(-4)),
+            (Integer(8) / -2, Integer(-4)),
             (Integer(7) / 2, Rational(7, 2)),
             (
+                # Here we test using ListExpression() instead of to_mathics_list()
                 ListExpression(Integer1, Integer(9))
                 / ListExpression(IntegerM1, Integer3),
                 ListExpression(IntegerM1, Integer3),
@@ -76,7 +73,7 @@ class ArithmeticTest(unittest.TestCase):
         )
         self._testCases(cases)
 
-    def testFloorDiv(self):
+    def testFloorDivision(self):
         cases = (
             (Integer(8) // Integer2, Integer(4)),
             (Integer(8) // -Integer2, Integer(-4)),
@@ -84,7 +81,10 @@ class ArithmeticTest(unittest.TestCase):
         )
         self._testCases(cases)
 
-    def testPow(self):
+    def testPower(self):
+        """
+        Test forms of binary infix exponentiation operator
+        """
         cases = (
             (Integer(8) ** Integer2, Integer(64)),
             (
@@ -96,6 +96,9 @@ class ArithmeticTest(unittest.TestCase):
         self._testCases(cases)
 
     def testAbs(self):
+        """
+        Test forms of binary absolute value function
+        """
         cases = (
             (abs(Integer(-8)), Integer(8)),
             (abs(ListExpression()), Expression(SymbolAbs, ListExpression())),
@@ -105,8 +108,12 @@ class ArithmeticTest(unittest.TestCase):
     def _testCases(self, cases):
         for expression, result in cases:
             self.assertEqual(
-                Expression("FullSimplify", expression).evaluate(self.evaluation),
-                Expression("FullSimplify", result).evaluate(self.evaluation),
+                # Use both Expression() ...
+                Expression(Symbol("FullSimplify"), expression).evaluate(
+                    self.evaluation
+                ),
+                # and to_expression()
+                to_expression("FullSimplify", result).evaluate(self.evaluation),
             )
 
 
