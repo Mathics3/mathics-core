86bade378c459c5695ba18eb2c2fd90df3f23273
rocky <rb@dustyfeet.com>
2022-06-01 20:54:48 -0400

    Latest results
    Make sure we arent doing conversions in Expression()

--- mathics/core/expression.py
+++ mathics/core/expression.py
@@ -210,15 +210,10 @@ class Expression(BaseElement, NumericOperators, EvalMixin):
         #          from trepan.api import debug; debug()
         #     assert isinstance(element, BaseElement)
 
-        # Note: After we make a pass over all Expression() calls, these lines will get removed
-        # and replaced with the two commented-out lines below them:
-
-        self._elements, self.elements_properties = convert_expression_elements(
-            elements, from_python
-        )
-        assert isinstance(self._elements, tuple)
-        # self._elements = elements
-        # self.elements_properties = elements_properties
+        # self._elements, self.elements_properties = convert_expression_elements(
+        #     elements, from_python
+        # )
+        # assert isinstance(self._elements, tuple)
 
         self._sequences = None
         self._cache = None
