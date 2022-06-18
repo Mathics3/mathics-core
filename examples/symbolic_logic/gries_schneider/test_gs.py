5c222396fda141dc7f7024087d1aa295620ce637
rocky <rb@dustyfeet.com>
2022-06-18 09:34:19 -0400

    Set up Gries Schnieder for benchmark testing.

--- examples/symbolic_logic/gries_schneider/test_gs.py
+++ examples/symbolic_logic/gries_schneider/test_gs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -8,7 +8,7 @@ from mathics.core.parser import MathicsSingleLineFeeder, parse
 
 definitions = Definitions(add_builtin=True)
 
-for i in range(0, 4):
+for i in range(4):
     evaluation = Evaluation(definitions=definitions, catch_interrupt=False)
 
     expr = parse(definitions, MathicsSingleLineFeeder(f"<< GS{i}.m"))
