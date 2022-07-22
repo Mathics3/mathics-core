d60481cfc291bb5247c7e2319a5d8f6d32564eaf
rocky <rb@dustyfeet.com>
2022-07-22 12:59:21 -0400

    Small import bug and a small Doctest markup change

--- mathics/builtin/intfns/combinatorial.py
+++ mathics/builtin/intfns/combinatorial.py
@@ -243,8 +243,8 @@ class RogersTanimotoDissimilarity(_BooleanDissimilarity):
     <dl>
       <dt>'RogersTanimotoDissimilarity[$u$, $v$]'
       <dd>returns the Rogers-Tanimoto dissimilarity between the two boolean 1-D lists $u$ and $v$,
-      which is defined as R / (c_tt + c_ff + R) where n is len($u$), c_ij is
-      the number of occurrences of $u$[k]=i and $v$[k]=j for k<n, and R = 2 * (c_tf + c_ft).
+      which is defined as $R$ / (c_tt + c_ff + $R$) where $n$ is len($u$), c_ij is
+      the number of occurrences of $u$[$k$]=$i$ and $v$[$k]$=$j$ for $k$<n, and $R$ = 2 * (c_tf + c_ft).
     </dl>
 
     >> RogersTanimotoDissimilarity[{1, 0, 1, 1, 0, 1, 1}, {0, 1, 1, 0, 0, 0, 1}]
