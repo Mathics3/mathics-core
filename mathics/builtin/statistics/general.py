852290326ade3d10bde0efbc2bb03b7b6d1c39c6
rocky <rb@dustyfeet.com>
2022-07-23 11:33:51 -0400

    Some more of the same

--- mathics/builtin/statistics/general.py
+++ mathics/builtin/statistics/general.py
@@ -12,12 +12,12 @@ from mathics.builtin.base import Builtin
 
 class CentralMoment(Builtin):
     """
+    Central moment. See <url>https://en.wikipedia.org/wiki/Central_moment</url>.
+
     <dl>
       <dt>'CentralMoment[$list$, $r$]'
       <dd>gives the the $r$th central moment (i.e. the $r$th moment about the mean) of $list$.
     </dl>
-
-    See <url>https://en.wikipedia.org/wiki/Central_moment</url>.
 
     >> CentralMoment[{1.1, 1.2, 1.4, 2.1, 2.4}, 4]
      = 0.100845
