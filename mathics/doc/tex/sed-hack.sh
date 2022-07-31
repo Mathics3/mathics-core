42dc55e2f8530f00326a1d53c5c3c3ff31b7bd3d
rocky <rb@dustyfeet.com>
2022-07-30 23:10:40 -0400

    save a copy before messing with documentation.tex

--- mathics/doc/tex/sed-hack.sh
+++ mathics/doc/tex/sed-hack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Brute force convert Unicode characters in LaTeX that it can't handle
+cp documentation.tex{,-before-sed}
 sed -i -e s/π/\\\\pi/ documentation.tex
 sed -i -e s/“/\`\`/ documentation.tex
 sed -i -e s/”/''/ documentation.tex
