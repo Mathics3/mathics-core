b07acfd070e451219b3773c5b16a8a665433c3e1
rocky <rb@dustyfeet.com>
2022-08-06 18:04:12 -0400

    Remove unused functions in core.util

--- mathics/core/util.py
+++ mathics/core/util.py
@@ -11,50 +11,9 @@ except ImportError:
     Pattern = re._pattern_type
 
 
-FORMAT_RE = re.compile(r"\`(\d*)\`")
-
 IS_PYPY = "__pypy__" in sys.builtin_module_names
 
-
-def dict_with_escaped_keys(d: dict) -> dict:
-    """Takes a dictionary and returns a copy of it where the keys are escaped
-    with re.escape"""
-    return {re.escape(k): v for k, v in d.items()}
-
-
-def interpolate_string(text, get_param) -> str:
-    index = [1]
-
-    def get_item(index):
-        if 1 <= index <= len(args):
-            return args[index - 1]
-        else:
-            return ""
-
-    if isinstance(get_param, list):
-        args = get_param
-        get_param = get_item
-
-    def repl(match):
-        arg = match.group(1)
-        if arg == "" or arg == "0":
-            arg = index[0]
-        else:
-            arg = int(arg)
-        index[0] += 1
-        param = get_param(arg)
-        return param
-
-    return FORMAT_RE.sub(repl, text)
-
-
-"""
-NOTE: Maybe see
-http://www.cosc.canterbury.ac.nz/tad.takaoka/isaac.pdf
-resp.
-http://www.cosc.canterbury.ac.nz/tad.takaoka/perm.p
-for a permutation generating algorithm for multisets.
-"""
+# FIXME: These functions are used pattern.py
 
 
 def permutations(items, without_duplicates=True):
@@ -69,14 +28,6 @@ def permutations(items, without_duplicates=True):
         for sub in permutations(items[:index] + items[index + 1 :]):
             yield [item] + sub
             # already_taken.add(item)
-
-
-def re_from_keys(d: dict) -> Pattern:
-    """Returns a regex that matches any of the keys of the dictionary"""
-
-    # The keys are sorted to prevent shorter keys from obscuring longer keys
-    # when pattern matching
-    return re.compile("|".join(sorted(d.keys(), key=lambda k: (-len(k), k))))
 
 
 def subsets(items, min, max, included=None, less_first=False):
@@ -110,53 +61,6 @@ def subsets(items, min, max, included=None, less_first=False):
             yield chosen, ([], not_chosen)
 
 
-def subsets_2(items, min, max, without_duplicates=True):
-    """max may only be 1 or None (= infinity).
-    Respects include property of items
-    """
-
-    if min <= max == 1:
-        for index in range(len(items)):
-            if items[index].include:
-                yield [items[index]], ([], items[:index] + items[index + 1 :])
-        if min == 0:
-            yield [], ([], items)
-    else:
-        counts = {}
-        for item in items:
-            if item.include:
-                if item in counts:
-                    counts[item] += 1
-                else:
-                    counts[item] = 1
-        already = set()
-
-        def decide(chosen, not_chosen, rest):
-            if not rest:
-                if len(chosen) >= min:
-                    """if False and len(chosen) > 1 and (
-                            permutate_until is None or
-                            len(chosen) <= permutate_until):
-                        for perm in permutations(chosen):
-                            yield perm, ([], not_chosen)
-                    else:"""
-                    yield chosen, ([], not_chosen)
-            else:
-                if rest[0].include:
-                    for set in decide(chosen + [rest[0]], not_chosen, rest[1:]):
-                        yield set
-                for set in decide(chosen, not_chosen + [rest[0]], rest[1:]):
-                    yield set
-
-        for subset in decide([], [], list(counts.keys())):
-            t = tuple(subset[0])
-            if t not in already:
-                yield subset
-                already.add(t)
-            else:
-                print("already taken")
-
-
 def subranges(
     items, min_count, max, flexible_start=False, included=None, less_first=False
 ):
@@ -181,26 +85,3 @@ def subranges(
                 items[start : start + length],
                 (items[:start], items[start + length :]),
             )
-
-
-def unicode_superscript(value) -> str:
-    def repl_char(c):
-        if c == "1":
-            value = 185
-        elif c == "2":
-            value = 178
-        elif c == "3":
-            value = 179
-        elif "0" <= c <= "9":
-            value = 8304 + (ord(c) - ord("0"))
-        elif c == "-":
-            value = 8315
-        elif c == "(":
-            value = 8317
-        elif c == ")":
-            value = 8318
-        else:
-            value = ord(c)
-        return chr(value)
-
-    return "".join(repl_char(c) for c in value)
