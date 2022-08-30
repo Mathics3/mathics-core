1feb84a57f65d5a4e28d518c70449e1f91aea582
mmatera <matera@fisica.unlp.edu.ar>
2022-08-30 06:24:53 -0300

    adding pytest

--- test/builtin/atomic/test_strings.py
+++ test/builtin/atomic/test_strings.py
@@ -29,3 +29,50 @@ def test_alphabet(str_expr, str_expected, fail_msg, warnings):
     check_evaluation(
         str_expr, str_expected, failure_message="", expected_messages=warnings
     )
+
+
+@pytest.mark.parametrize(
+    ("str_expr", "str_expected", "fail_msg", "warnings"),
+    [
+        (r'ToString["\[Integral]", CharacterEncoding->"Unicode"]', r'"∫"', None, None),
+        (
+            r'ToString["\[Integral]", CharacterEncoding->"ASCII"]',
+            # FIXME:
+            # "\\[Integral]" is parsed as "\u222b" instead of "\[Integral]"
+            r'"\\[Inte" <> "gral]"',
+            None,
+            None,
+        ),
+        (
+            r'ToString["\[Integral]", CharacterEncoding->"No"]',
+            r'"∫"',
+            None,
+            [
+                "The character encoding No is not supported. Use $CharacterEncodings to list supported encodings."
+            ],
+        ),
+        (
+            r'ToString["\[Integral]", CharacterEncoding->"3"]',
+            r'"∫"',
+            None,
+            [
+                "The character encoding 3 is not supported. Use $CharacterEncodings to list supported encodings."
+            ],
+        ),
+        (
+            r'ToString["\[Integral]", TeXForm]',
+            r'"\\text{∫}"',  # Should be '\\int'
+            None,
+            None,
+        ),
+    ],
+)
+def test_tostring(str_expr, str_expected, fail_msg, warnings):
+    check_evaluation(
+        str_expr,
+        str_expected,
+        failure_message="",
+        expected_messages=warnings,
+        to_string_expr=False,
+        to_string_expected=False,
+    )
