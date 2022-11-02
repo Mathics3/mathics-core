c49f4064d472a39e4eb2e9f5adf78761ecd2277c
rocky <rb@dustyfeet.com>
2022-11-02 06:28:09 -0400

    More correct Mathics code for CuebRoot
    My WL skills are not that good.

--- mathics/autoload/forms/StandardForm.m
+++ mathics/autoload/forms/StandardForm.m
@@ -10,12 +10,15 @@ Begin["System`"]
 (* Change RadBox to RadicalBox. We use RadBox to make it clear that
    the below code was a read-in from a file and not some pre-existing
    code. *)
-RadicalBox[expr_, form_] = RadBox[MakeBoxes[expr, form], 3];
+Attributes[CommonRadicalBox] = HoldAll;
+Attributes[RadBox] = HoldAll;
+CommonRadicalBox[expr_, form_] = RadBox[MakeBoxes[expr, form], 3];
 
 (******************************************************************************************)
 (* StandardForm Boxing Rules                                                               *)
 (******************************************************************************************)
 
-MakeBoxes[CubeRoot[expr_], StandardForm] := RadicalBox[expr_, form_];
+MakeBoxes[CubeRoot[expr_], StandardForm] := CommonRadicalBox[expr, StandardForm];
 (*All the other StandardForm boxing routines... *)
+
 End[]
