6fda837cb41d2b2e483bc289adb695fe2f72abfd
rocky <rb@dustyfeet.com>
2022-11-11 07:01:35 -0500

    Update 'attributes' for current standards
    `mathics.builtin.attributes`:
    
    * apply -> eval
    * Add WMA links add
    * Class names ordered alphabetically
    * Hard breaks in docstring removed
    * Some incorrect references to "leaves" changed to "attributes"
    
    `mathics.core.attributes`:
    
    * Add short comments above flag value

--- mathics/core/attributes.py
+++ mathics/core/attributes.py
@@ -9,37 +9,73 @@
 # (the most of the cases).
 
 # To check if a builtin has an attribute, you do:
-# ATTRIBUTE_NAME & attributes
+#   ATTRIBUTE_NAME & attributes
+#
 # To set all the attributes of a builtin you do:
-# attributes = ATTRIBUTE1 | ATTRIBUTE2 | ATTRIBUTE3 | ...
+#   attributes = ATTRIBUTE1 | ATTRIBUTE2 | ATTRIBUTE3 | ...
+#
 # To add an attribute to a builtin you do:
-# attributes = ATTRIBUTE_NAME | attributes
+#   attributes = ATTRIBUTE_NAME | attributes
+#
 # To remove an attribute you do:
-# attributes = ~ATTRIBUTE_NAME & attributes
+#   attributes = ~ATTRIBUTE_NAME & attributes
 
 from typing import Dict, Generator
 
 # fmt: off
 A_NO_ATTRIBUTES     = 0b0000000000000000
 
-# FIXME: remove lowercase after thes are no longer imported
-# alphabetical order
+# Symbol is a constant.
 A_CONSTANT          = 0b00000000000000001
+
+# Nested occurrences of a function should be automatically flattened.
 A_FLAT              = 0b00000000000000010
+
+# All arguments of a function should be left unevaluated.
 A_HOLD_ALL          = 0b00000000000000100
+
+# Includes the effects of 'HoldAll' and 'SequenceHold', and also
+# protects the function from being affected by the upvalues of any
+# arguments.
 A_HOLD_ALL_COMPLETE = 0b00000000000001000
+
+# First argument of a function should be left unevaluated.
 A_HOLD_FIRST        = 0b00000000000010000
+
+# All but the first argument of a function should be left unevaluated.
 A_HOLD_REST         = 0b00000000000100000
+
+# Function should be automatically applied to each element of a list.
 A_LISTABLE          = 0b00000000001000000
+
+# prevents attributes on a symbol from being modified.
 A_LOCKED            = 0b00000000010000000
+
+# Protects all arguments of a function from numeric evaluation
 A_N_HOLD_ALL        = 0b00000000100000000
+
+# Protects the first argument of a function from numeric evaluation.
 A_N_HOLD_FIRST      = 0b00000001000000000
+
+# Protects all but the first argument of a function from numeric evaluation.
 A_N_HOLD_REST       = 0b00000010000000000
+
+# Symbol is the head of a numeric function.
 A_NUMERIC_FUNCTION  = 0b00000100000000000
+
+# '$f$[$x$]' should be treated as equivalent to $x$ in pattern matching.
 A_ONE_IDENTITY      = 0b00001000000000000
+
+# elements should automatically be sorted into canonical order
 A_ORDERLESS         = 0b00010000000000000
+
+# Prevents values on a symbol from being modified.
 A_PROTECTED         = 0b00100000000000000
+
+# Prevents values on a symbol from being read.
 A_READ_PROTECTED    = 0b01000000000000000
+
+# prevents 'Sequence' objects from being spliced into a function's arguments
 A_SEQUENCE_HOLD     = 0b10000000000000000
 
 attribute_number_to_string: Dict[int, str] = {
