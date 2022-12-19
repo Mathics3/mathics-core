3f2f1c28869fb3f85235b86562aabba8f68cb3a9
rocky <rb@dustyfeet.com>
2022-12-18 20:16:06 -0500

    Numpy 2.4 doesn't seem to work on 3.8
    Is this new or was this always the case?

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ elif sys.version_info[:2] == (3, 6):
 elif sys.version_info[:2] == (3, 7):
     INSTALL_REQUIRES += ["numpy<1.22", "llvmlite", "sympy>=1.8, < 1.11"]
 else:
-    INSTALL_REQUIRES += ["numpy", "llvmlite", "sympy>=1.8, < 1.11"]
+    INSTALL_REQUIRES += ["numpy<1.24", "llvmlite", "sympy>=1.8, < 1.11"]
 
 if not is_PyPy:
     INSTALL_REQUIRES += ["recordclass"]
