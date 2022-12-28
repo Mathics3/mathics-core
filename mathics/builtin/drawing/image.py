b66935c585f9f1d08f9b1b65d0d5ca9f26a3adaa
rocky <rb@dustyfeet.com>
2022-12-28 07:13:10 -0500

    Add ImageTake rows, row+cols forms

--- mathics/builtin/drawing/image.py
+++ mathics/builtin/drawing/image.py
@@ -1636,7 +1636,7 @@ class ImageData(_ImageBuiltin):
 
 class ImageTake(_ImageBuiltin):
     """
-    <url>:WMA link:
+    Crop Image <url>:WMA link:
     https://reference.wolfram.com/language/ref/ImageTake.html</url>
     <dl>
       <dt>'ImageTake[$image$, $n$]'
@@ -1659,9 +1659,27 @@ class ImageTake(_ImageBuiltin):
     Now crop to the include the lower half of that image:
     >> ImageTake[alice, -244]
      = -Image-
+
+    Just the text around the hat:
+    >> ImageTake[alice, {40, 150}, {500, 600}]
+     = -Image-
+
     """
 
     summary_text = "crop image"
+
+    def _slice(self, image, i1: Integer, i2: Integer, axis):
+        n = image.pixels.shape[axis]
+        py_i1 = min(max(i1.value - 1, 0), n - 1)
+        py_i2 = min(max(i2.value - 1, 0), n - 1)
+
+        def flip(pixels):
+            if py_i1 > py_i2:
+                return numpy_flip(pixels, axis)
+            else:
+                return pixels
+
+        return slice(min(py_i1, py_i2), 1 + max(py_i1, py_i2)), flip
 
     def eval(self, image, n: Integer, evaluation: Evaluation):
         "ImageTake[image_Image, n_Integer]"
@@ -1683,31 +1701,95 @@ class ImageTake(_ImageBuiltin):
 
         return Image(pixels, image.color_space, pillow=pillow)
 
-    def _slice(self, image, i1: Integer, i2: Integer, axis):
-        n = image.pixels.shape[axis]
-        py_i1 = min(max(i1.value - 1, 0), n - 1)
-        py_i2 = min(max(i2.value - 1, 0), n - 1)
-
-        def flip(pixels):
-            if py_i1 > py_i2:
-                return numpy_flip(pixels, axis)
-            else:
-                return pixels
-
-        return slice(min(py_i1, py_i2), 1 + max(py_i1, py_i2)), flip
-
     def eval_rows(self, image, r1: Integer, r2: Integer, evaluation: Evaluation):
         "ImageTake[image_Image, {r1_Integer, r2_Integer}]"
-        s, f = self._slice(image, r1, r2, 0)
-        return Image(f(image.pixels[s]), image.color_space)
+
+        first_row = r1.value
+        last_row = r2.value
+
+        max_row, max_col = image.pixels.shape[:2]
+        adjusted_first_row = (
+            min(first_row, max_row) if first_row > 0 else max(0, max_row + first_row)
+        )
+        adjusted_last_row = (
+            min(last_row, max_row) if last_row > 0 else max(0, max_row + first_row)
+        )
+
+        # More complicated in that it reverses the data?
+        # if adjusted_first_row > adjusted_last_row:
+        #     adjusted_first_row, adjusted_last_row = adjusted_last_row, adjusted_first_row
+
+        pixels = image.pixels[adjusted_first_row:adjusted_last_row]
+
+        if hasattr(image, "pillow"):
+            box_coords = (0, adjusted_first_row, max_col, adjusted_last_row)
+            pillow = image.pillow.crop(box_coords)
+            pixels = numpy.asarray(pillow)
+            return Image(pixels, image.color_space, pillow=pillow)
+
+        pixels = image.pixels[adjusted_first_row:adjusted_last_row]
+        return Image(pixels, image.color_space, pillow=pillow)
 
     def eval_rows_cols(
         self, image, r1: Integer, r2: Integer, c1: Integer, c2: Integer, evaluation
     ):
         "ImageTake[image_Image, {r1_Integer, r2_Integer}, {c1_Integer, c2_Integer}]"
-        sr, fr = self._slice(image, r1, r2, 0)
-        sc, fc = self._slice(image, c1, c2, 1)
-        return Image(fc(fr(image.pixels[sr, sc])), image.color_space)
+
+        first_row = r1.value
+        last_row = r2.value
+        first_col = c1.value
+        last_col = c2.value
+
+        max_row, max_col = image.pixels.shape[:2]
+        adjusted_first_row = (
+            min(first_row, max_row) if first_row > 0 else max(0, max_row + first_row)
+        )
+        adjusted_last_row = (
+            min(last_row, max_row) if last_row > 0 else max(0, max_row + last_row)
+        )
+        adjusted_first_col = (
+            min(first_col, max_col) if first_col > 0 else max(0, max_col + first_col)
+        )
+        adjusted_last_col = (
+            min(last_col, max_col) if last_col > 0 else max(0, max_col + last_col)
+        )
+
+        # if adjusted_first_row > adjusted_last_row:
+        #     adjusted_first_row, adjusted_last_row = adjusted_last_row, adjusted_first_row
+
+        # if adjusted_first_col > adjusted_last_col:
+        #     adjusted_first_col, adjusted_last_col = adjusted_last_col, adjusted_first_col
+
+        pixels = image.pixels[
+            adjusted_first_col:adjusted_last_col, adjusted_last_row:adjusted_last_row
+        ]
+
+        if hasattr(image, "pillow"):
+            box_coords = (
+                adjusted_first_col,
+                adjusted_first_row,
+                adjusted_last_col,
+                adjusted_last_row,
+            )
+            pillow = image.pillow.crop(box_coords)
+            pixels = numpy.asarray(pillow)
+            return Image(pixels, image.color_space, pillow=pillow)
+
+        pixels = image.pixels[adjusted_first_row:adjusted_last_row]
+        return Image(pixels, image.color_space, pillow=pillow)
+
+    # def eval_rows(self, image, r1: Integer, r2: Integer, evaluation: Evaluation):
+    #     "ImageTake[image_Image, {r1_Integer, r2_Integer}]"
+    #     s, f = self._slice(image, r1, r2, 0)
+    #     return Image(f(image.pixels[s]), image.color_space)
+
+    # def eval_rows_cols(
+    #     self, image, r1: Integer, r2: Integer, c1: Integer, c2: Integer, evaluation
+    # ):
+    #     "ImageTake[image_Image, {r1_Integer, r2_Integer}, {c1_Integer, c2_Integer}]"
+    #     sr, fr = self._slice(image, r1, r2, 0)
+    #     sc, fc = self._slice(image, c1, c2, 1)
+    #     return Image(fc(fr(image.pixels[sr, sc])), image.color_space)
 
 
 class PixelValue(_ImageBuiltin):
