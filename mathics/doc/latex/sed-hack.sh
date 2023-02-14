dbc883aca1d83aeb3dfa60b6c23faa0354b8c703
rocky <rb@dustyfeet.com>
2023-02-14 04:02:13 -0500

    A bunch of documentation fixes/workarounds
    In some cases we are just putting back the ASCII equivalents of
    unicode operators, until we can get MakeBoxes to do the right thing.
    
    Improve BinarySearch formatting and example commentary
    
    Makefile: always do the sed hack after generating LaTeX file

--- mathics/doc/latex/sed-hack.sh
+++ mathics/doc/latex/sed-hack.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x
 # Brute force convert Unicode characters in LaTeX that it can't handle
+# Workaround for messages of the form:
+#    Missing character: There is no ⩵ ("2A75) in font pplr7t!
+# Mathics3 MakeBox rules should handle this but they don't.
+
+# Characters that only work in math mode we convert back
+# to their ASCII equivalent. Otherwise, since we don't
+# understand context, it might not be right to
+# use a math-mode designation.
 if [[ -f documentation.tex ]] ; then
     cp documentation.tex{,-before-sed}
 fi
@@ -12,5 +20,20 @@ sed -i -e s/″/''/g documentation.tex
 # sed -i -e s/\\′/'/g documentation.text
 #sed -i -e s/′/'/ documentation.tex
 sed -i -e s/μ/$\\\\mu$/g documentation.tex
-sed -i -e s/–/--/g documentation.tex
-sed -i -e s/Φ/$\\\\Phi$/g documentation.tex
+sed -i -e 's/–/--/g' documentation.tex
+sed -i -e 's/Φ/$\\\\Phi$/g' documentation.tex
+sed -i -e 's/ϕ/Phi/g' documentation.tex
+sed -i -e 's/→/->/g' documentation.tex
+sed -i -e 's/→/->/g' documentation.tex
+sed -i -e 's/⧴/:>/g' documentation.tex
+sed -i -e 's/—/-/g' documentation.tex
+sed -i -e 's/≤/<=/g' documentation.tex
+sed -i -e 's/≠/!=/g' documentation.tex
+sed -i -e 's/⩵/==/g' documentation.tex
+sed -i -e 's/∧/&&/g' documentation.tex
+sed -i -e 's/⧦/\\\\Equiv/g' documentation.tex
+sed -i -e 's/⊻/xor/g' documentation.tex
+sed -i -e 's/∧/&&/g' documentation.tex
+# This may looks he same as the above, but it is different.
+sed -i -e 's/∧/&&/g' documentation.tex
+sed -i -e 's/‖/||/g' documentation.tex
