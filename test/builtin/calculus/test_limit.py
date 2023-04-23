01c84c75bcf6029fb5ba32b4d23d678c40819303
rocky <rb@dustyfeet.com>
2023-04-23 19:10:39 -0400

    Reinstate Limit rules...
    thanks to mmatera for the fix.
    
    More work is needed so we don't have to do this kind of thing though.

--- test/builtin/calculus/test_limit.py
+++ test/builtin/calculus/test_limit.py
@@ -10,15 +10,14 @@ import pytest
 @pytest.mark.parametrize(
     ("str_expr", "str_expected", "msg"),
     [
-        # Defining limit rules is broken! FIXME
-        # ("Limit[Tan[x], x->Pi/2]", "Indeterminate", None),
-        # ("Limit[Cot[x], x->0]", "Indeterminate", None),
-        # ("Limit[x*Sqrt[2*Pi]^(x^-1)*(Sin[x]/(x!))^(x^-1), x->Infinity]", "E", None),
+        ("Limit[Tan[x], x->Pi/2]", "Indeterminate", None),
+        ("Limit[Cot[x], x->0]", "Indeterminate", None),
+        ("Limit[x*Sqrt[2*Pi]^(x^-1)*(Sin[x]/(x!))^(x^-1), x->Infinity]", "E", None),
         (
             "Limit[x, x -> x0, Direction -> x]",
             "Limit[x, x -> x0, Direction -> x]",
             "Value of Direction -> x should be -1 or 1.",
-        )
+        ),
     ],
 )
 def test_limit(str_expr, str_expected, msg):
