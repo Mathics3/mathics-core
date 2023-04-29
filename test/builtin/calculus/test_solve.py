ec822ac00a1ae2c8f0f6359d6962904e5b6dbe4e
rocky <rb@dustyfeet.com>
2023-04-29 05:23:43 -0400

    Make explicit the need for serialization ...
    in Definition and Rule.
    
    Some small linting was doine to shorten long lines,
    remove an uneeded import, correct a type annotation here and there.
    
    test_serialization was expanded to include a rule test.
    
    test_calculus was split and moved into test/builtin/calculus.

--- test/builtin/calculus/test_solve.py
+++ test/builtin/calculus/test_solve.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Unit tests from builtins ... calculus.py
+Unit tests for mathics.builtins.calculus.Solve
+
 """
 
-from .helper import check_evaluation, session
+from test.helper import check_evaluation, session
 
 
-def test_calculus():
+def test_solve():
     for str_expr, str_expected, message in (
         (
             "Solve[{(7+x)*ma == 167, (5+x)*mb == 167, (7+5)*(ma+mb) == 334}, {ma, mb, x}]",
@@ -37,17 +38,6 @@ def test_calculus():
             "v1 := Exp[x] - 3x; v2 = {x, 2}; FindRoot[v1, v2]",
             "{x->1.51213}",
             "Issue #1235",
-        ),
-        ("Integrate[Integrate[1,{y,0,E^x}],{x,0,Log[13]}]", "12", "Issue #153"),
-        (
-            "g/:Integrate[g[u_],u_]:=f[u]; Integrate[g[x],x]",
-            "f[x]",
-            "This should pass after implementing an earlier sympy evaluation.",
-        ),
-        (
-            "h=x;Integrate[Do[h=x*h,{5}]; h,x]",
-            "x^7/7",
-            "another sanity check for a more agressive sympy translation.",
         ),
     ):
         session.evaluate("Clear[h]; Clear[g]; Clear[f];")
