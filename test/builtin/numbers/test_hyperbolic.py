8265c30a47a3e3416758bca18b4d5f15a578e522
mmatera <matera@fisica.unlp.edu.ar>
2023-09-09 23:18:38 -0300

    move private doctests to pytests in mathics.builtin.numbers

--- test/builtin/numbers/test_hyperbolic.py
+++ test/builtin/numbers/test_hyperbolic.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 """
-Unit tests for mathics.builtins.numbers.hyperbolic
+Unit tests for mathics.builtins.numbers.hyperbolic and 
+mathics.builtins.numbers.exp
 
 These simple verify various rules from
 from symja_android_library/symja_android_library/rules/Gudermannian.m
 """
 from test.helper import check_evaluation
+
+import pytest
 
 
 def test_gudermannian():
@@ -34,3 +37,53 @@ def test_complexexpand():
         ),
     ):
         check_evaluation(str_expr, str_expected)
+
+
+@pytest.mark.parametrize(
+    ("str_expr", "msgs", "str_expected", "fail_msg"),
+    [
+        ("ArcCosh[1.4]", None, "0.867015", None),
+        (
+            "ArcCoth[0.000000000000000000000000000000000000000]",
+            None,
+            "1.57079632679489661923132169163975144210 I",
+            None,
+        ),
+    ],
+)
+def test_private_doctests_hyperbolic(str_expr, msgs, str_expected, fail_msg):
+    """ """
+    check_evaluation(
+        str_expr,
+        str_expected,
+        to_string_expr=True,
+        to_string_expected=True,
+        hold_expected=True,
+        failure_message=fail_msg,
+        expected_messages=msgs,
+    )
+
+
+@pytest.mark.parametrize(
+    ("str_expr", "msgs", "str_expected", "fail_msg"),
+    [
+        ("Exp[1.*^20]", ("Overflow occurred in computation.",), "Overflow[]", None),
+        ("Log[1000] / Log[10] // Simplify", None, "3", None),
+        ("Log[1.4]", None, "0.336472", None),
+        ("Log[Exp[1.4]]", None, "1.4", None),
+        ("Log[-1.4]", None, "0.336472 + 3.14159 I", None),
+        ("N[Log[10], 30]", None, "2.30258509299404568401799145468", None),
+        ("LogisticSigmoid[I Pi]", None, "LogisticSigmoid[I Pi]", None),
+    ],
+)
+def test_private_doctests_exp(str_expr, msgs, str_expected, fail_msg):
+    """ """
+    check_evaluation(
+        str_expr,
+        str_expected,
+        to_string_expr=True,
+        to_string_expected=True,
+        hold_expected=True,
+        failure_message=fail_msg,
+        expected_messages=msgs,
+    )
