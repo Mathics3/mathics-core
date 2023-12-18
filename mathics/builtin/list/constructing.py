c520987ff4c31a317cbc0c36dcd652dc821838e3
Li-Xiang-Ideal <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2023-12-18 20:56:34 +0800

    Add description for Range

--- mathics/builtin/list/constructing.py
+++ mathics/builtin/list/constructing.py
@@ -213,6 +213,12 @@ class Range(Builtin):
 
       <dt>'Range[$a$, $b$]'
       <dd>returns a list of integers from $a$ to $b$.
+
+      <dt>'Range[$a$, $b$, $di$]'
+      <dd>returns a list of integers from $a$ to $b$ using step $di$.
+        More specifically, 'Range' starts from $a$ and successively adds \
+        increments of $di$ until the result is greater (if $di$ > 0) or \
+        less (if $di$ < 0) than $b$.
     </dl>
 
     >> Range[5]
