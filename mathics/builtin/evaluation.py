67ddf52b0aa34d315aef997f0b6b9253dd0e56ab
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2024-02-19 08:00:41 -0300

    Update evaluation.py

--- mathics/builtin/evaluation.py
+++ mathics/builtin/evaluation.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-# """
-# Mathics3 takes an expression that it is given, and evaluates it. Built \
-# into the evaluation are primitives that allow finer control over the \
-# process of evaluation in cases where it is needed.
-# """
-
 from mathics.core.atoms import Integer
 from mathics.core.attributes import A_HOLD_ALL, A_HOLD_ALL_COMPLETE, A_PROTECTED
 from mathics.core.builtin import Builtin, Predefined
