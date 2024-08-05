50cfce1d36791b427b58aea70feba14ac71489dd
rocky <rb@dustyfeet.com>
2024-08-05 14:55:50 -0400

    More doc-related fixes...
    Add another "\n" at the end of a </dl>
    
    Compute number boxwidth automatically in tocbox numbers

--- mathics/builtin/graphics.py
+++ mathics/builtin/graphics.py
@@ -485,7 +485,9 @@ class PointSize(_Size):
       <dt>'PointSize[$t$]'
       <dd>sets the diameter of points to $t$, which is relative to the overall width.
     </dl>
-    'PointSize' can be used for both two- and three-dimensional graphics. The initial default pointsize is 0.008 for two-dimensional graphics and 0.01 for three-dimensional graphics.
+
+    'PointSize' can be used for both two- and three-dimensional graphics. \
+    The initial default pointsize is 0.008 for two-dimensional graphics and 0.01 for three-dimensional graphics.
 
     >> Table[Graphics[{PointSize[r], Point[{0, 0}]}], {r, {0.02, 0.05, 0.1, 0.3}}]
      = {-Graphics-, -Graphics-, -Graphics-, -Graphics-}
