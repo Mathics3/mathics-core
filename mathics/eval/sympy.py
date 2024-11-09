0f99352848820ee9be93faa30f2d4d9c475098ce
mmatera <matera@fisica.unlp.edu.ar>
2024-11-09 09:58:57 -0300

    using simplified eval_sympy function for Pyodide

--- mathics/eval/sympy.py
+++ mathics/eval/sympy.py
@@ -12,34 +12,47 @@ import mathics.eval.tracing as tracing
 from mathics.core.convert.sympy import from_sympy, to_numeric_sympy_args
 
 
-def eval_sympy(self, z, evaluation):
+def eval_sympy_unconstrained(self, z, evaluation):
     """
-    Evaluate expr using `Sympy`
+    Evaluate an expression converting it to Sympy
+    and back to Mathics.
     """
+    sympy_args = to_numeric_sympy_args(z, evaluation)
+    if self.sympy_name is None:
+        return
+    sympy_fn = getattr(sympy, self.sympy_name)
+    try:
+        return from_sympy(tracing.run_sympy(sympy_fn, *sympy_args))
+    except Exception:
+        return
 
-    def evaluate():
-        sympy_args = to_numeric_sympy_args(z, evaluation)
-        if self.sympy_name is None:
-            return
-        sympy_fn = getattr(sympy, self.sympy_name)
-        try:
-            return from_sympy(tracing.run_sympy(sympy_fn, *sympy_args))
-        except Exception:
-            return
+
+def eval_sympy_with_timeout(self, z, evaluation):
+    """
+    Evaluate an expression converting it to Sympy,
+    and back to Mathics.
+    This version put the evaluation in a thread,
+    and check each some time if the evaluation
+    reached a timeout.
+    """
 
     if evaluation.timeout is None:
-        return evaluate()
+        return eval_sympy_unconstrained(self, z, evaluation)
 
-    def _thread_target(request, queue) -> None:
+    def _thread_target(queue) -> None:
         try:
-            result = evaluate()
+            result = eval_sympy_unconstrained(self, z, evaluation)
             queue.put((True, result))
         except BaseException:
             exc_info = sys.exc_info()
             queue.put((False, exc_info))
 
     queue = Queue(maxsize=1)  # stores the result or exception
-    thread = Thread(target=_thread_target, args=(evaluate, queue))
+
+    def evaluate():
+        return
+
+    thread = Thread(target=_thread_target, args=(queue,))
     thread.start()
     while thread.is_alive():
         thread.join(0.001)
@@ -54,3 +67,10 @@ def eval_sympy(self, z, evaluation):
         return result
     else:
         raise result[0].with_traceback(result[1], result[2])
+
+
+eval_sympy = (
+    eval_sympy_unconstrained
+    if sys.platform in ("emscripten",)
+    else eval_sympy_with_timeout
+)
