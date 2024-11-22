bac8e035d23d8b2ab0d97612864fd78cd1e70814
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2024-11-22 12:45:53 +0100

    Run isort and black.

--- test/core/test_patterns.py
+++ test/core/test_patterns.py
@@ -9,33 +9,34 @@ from test.helper import check_evaluation, evaluate
 
 import pytest
 
+
 @pytest.mark.parametrize(
     ("str_expr", "msgs", "str_expected", "fail_msg"),
     [
         # Two default arguments (linear)
-        ("MatchQ[1, a_.+b_.*x_]",None,"True",None),
-        ("MatchQ[x, a_.+b_.*x_]",None,"True",None),
-        ("MatchQ[2*x, a_.+b_.*x_]",None,"True",None),
-        ("MatchQ[1+x, a_.+b_.*x_]",None,"True",None),
-        ("MatchQ[1+2*x, a_.+b_.*x_]",None,"True",None),
+        ("MatchQ[1, a_.+b_.*x_]", None, "True", None),
+        ("MatchQ[x, a_.+b_.*x_]", None, "True", None),
+        ("MatchQ[2*x, a_.+b_.*x_]", None, "True", None),
+        ("MatchQ[1+x, a_.+b_.*x_]", None, "True", None),
+        ("MatchQ[1+2*x, a_.+b_.*x_]", None, "True", None),
         # Default argument (power)
-        ("MatchQ[1, x_^m_.]",None,"True",None),
-        ("MatchQ[x, x_^m_.]",None,"True",None),
-        ("MatchQ[x^1, x_^m_.]",None,"True",None),
-        ("MatchQ[x^2, x_^m_.]",None,"True",None),
+        ("MatchQ[1, x_^m_.]", None, "True", None),
+        ("MatchQ[x, x_^m_.]", None, "True", None),
+        ("MatchQ[x^1, x_^m_.]", None, "True", None),
+        ("MatchQ[x^2, x_^m_.]", None, "True", None),
         # Two default arguments (power)
-        ("MatchQ[1, x_.^m_.]",None,"True",None),
-        ("MatchQ[x, x_.^m_.]",None,"True",None),
-        ("MatchQ[x^1, x_.^m_.]",None,"True",None),
-        ("MatchQ[x^2, x_.^m_.]",None,"True",None),
+        ("MatchQ[1, x_.^m_.]", None, "True", None),
+        ("MatchQ[x, x_.^m_.]", None, "True", None),
+        ("MatchQ[x^1, x_.^m_.]", None, "True", None),
+        ("MatchQ[x^2, x_.^m_.]", None, "True", None),
         # Two default arguments (no non-head)
-        ("MatchQ[1, a_.+b_.]",None,"True",None),
-        ("MatchQ[x, a_.+b_.]",None,"True",None),
-        ("MatchQ[1+x, a_.+b_.]",None,"True",None),
-        ("MatchQ[1+2*x, a_.+b_.]",None,"True",None),
-        ("MatchQ[1, a_.+b_.]",None,"True",None),
-        ("MatchQ[x, a_.*b_.]",None,"True",None),
-        ("MatchQ[2*x, a_.*b_.]",None,"True",None),
+        ("MatchQ[1, a_.+b_.]", None, "True", None),
+        ("MatchQ[x, a_.+b_.]", None, "True", None),
+        ("MatchQ[1+x, a_.+b_.]", None, "True", None),
+        ("MatchQ[1+2*x, a_.+b_.]", None, "True", None),
+        ("MatchQ[1, a_.+b_.]", None, "True", None),
+        ("MatchQ[x, a_.*b_.]", None, "True", None),
+        ("MatchQ[2*x, a_.*b_.]", None, "True", None),
     ],
 )
 def test_patterns(str_expr, msgs, str_expected, fail_msg):
