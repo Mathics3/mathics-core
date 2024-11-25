06e80651c71d9f6e5224523769b891c4ebd166eb
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2024-11-25 23:56:09 +0100

    Apply suggestions from code review
    Co-authored-by: Juan Mauricio Matera <matera@fisica.unlp.edu.ar>

--- mathics/builtin/system.py
+++ mathics/builtin/system.py
@@ -885,8 +885,7 @@ class Breakpoint(Builtin):
     </dl>
 
     >> (* Test with a disabled breakpoint. *)
-    >> In[1]:= SetEnvironment["PYTHONBREAKPOINT" -> "mathics.disabled_breakpoint"]
-    Out[1]= SetEnvironment[PYTHONBREAKPOINT → mathics.disabled_breakpoint]
+    >> In[1]:= SetEnvironment["PYTHONBREAKPOINT" -> "mathics.disabled_breakpoint"];
     >> Breakpoint[]
     Hit disabled breakpoint.
     Out[2]= Breakpoint[]
