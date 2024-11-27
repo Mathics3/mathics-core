8a34c3c1f70d85b671e16b83b28ba03f625f1cbd
rocky <rb@dustyfeet.com>
2024-11-27 03:27:04 -0500

    DRY code to the extend that we can...
    Note: class creation code has to be in the respective modules for
    `type()` to put the newly created class in the proper module.
    
    This leads to a little bit of duplication of code, but on the other hand,
    it makes some tricky code easier to follow

--- mathics/builtin/no_meaning/infix.py
+++ mathics/builtin/no_meaning/infix.py
@@ -4,51 +4,47 @@ Infix Operators without Built-in Meanings
 
 from sys import modules
 
-import mathics.core.parser.operators
-from mathics.core.builtin import NoMeaningInfixOperator
-from mathics.core.parser.operators import OPERATOR_DATA
+from mathics.core.builtin import NoMeaningInfixOperator, add_no_meaning_builtin_classes
 
 
-def add_no_meaning_builtins(
-    affix: str, format_function_name: str, operator_base_class, builtin_module
-):
-    operator_key = f"no-meaning-{affix}-operators"
-    for operator_name, operator_tuple in OPERATOR_DATA[operator_key].items():
-        # Create the Mathics3 Builtin class...
-        operator_string = operator_tuple[0]
-        generated_operator_class = type(
-            operator_name,
-            (operator_base_class,),
-            {
-                "__doc__": operator_base_class.__doc_pattern__.format(
-                    operator_name=operator_name, operator_string=operator_string
-                ),
-                "operator": operator_string,
-                "summary_text": f"""{operator_name} {affix} operator "{operator_string}" (no pre-set meaning attached)""",
-                "formats": {
+# Note: this function has to exist inside this module, for the created modules
+# to be put under module `mathics.builtin.no_meaning.infix`. This seems to be
+# a limitation on the Python class-creation function type().
+def create_class_function(
+    operator_name: str,
+    operator_base_class,
+    operator_string: str,
+    mathics3_format_function_name: str,
+) -> type:
+    """
+    Returns a Mathics3 Builtin function associated with this module
+    (mathics.builtin.no_meaning.infix) that implements a Mathics infix operator
+    with no pre-existing meaning.
+    """
+    return type(
+        operator_name,
+        (operator_base_class,),
+        {
+            "__doc__": operator_base_class.__doc_pattern__.format(
+                operator_name=operator_name, operator_string=operator_string
+            ),
+            "operator": operator_string,
+            "summary_text": f"""{operator_name} postfix operator "{operator_string}" (no pre-set meaning attached)""",
+            "formats": {
+                (
+                    ("InputForm", "OutputForm", "StandardForm"),
+                    f"{operator_name}[args__]",
+                ): (
                     (
-                        ("InputForm", "OutputForm", "StandardForm"),
-                        f"{operator_name}[args__]",
-                    ): (('%s[{args}, "%s"]' % (format_function_name, operator_string)))
-                },
+                        '%s[{args}, "%s"]'
+                        % (mathics3_format_function_name, operator_string)
+                    )
+                )
             },
-        )
-
-        # FIXME: Right now, precedence for infix operators is listed in
-        # JSON only for infix operators. Extend table generation in the same way
-        # for prefix and postfix, and then extend the code below to
-        # assign operator precedences for prefix and postfix operators.
-        if affix == "infix":
-            mathics.core.parser.operators.flat_binary_ops[
-                operator_name
-            ] = operator_tuple[1]
-
-        # Put the newly-created Builtin class inside this module.
-        setattr(builtin_module, operator_name, generated_operator_class)
+        },
+    )
 
 
-def add_no_meaning_infix_operators():
-    add_no_meaning_builtins("infix", "Infix", NoMeaningInfixOperator, modules[__name__])
-
-
-add_no_meaning_infix_operators()
+add_no_meaning_builtin_classes(
+    create_class_function, "infix", "Infix", NoMeaningInfixOperator, modules[__name__]
+)
