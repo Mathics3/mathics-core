a039f10f87e13ebde59fa60e1e598024ae2c8aeb
R. Bernstein <rocky@users.noreply.github.com>
2024-12-01 18:50:03 -0500

    Improve cosine distance (#1199)
    Handle CosineDistance more correctly in symbolic computations that should use Conjugate. Code adapted from
    https://github.com/axkr/symja_android_library/blob/master/symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/ClusteringFunctions.java#L377-L386
    
    However, we also add the situation where arguments are numeric scalars, and we check for mismatched vector lengths.
    
    Some tweaking of code for conversion to/from Complex has been done. 
    
    TODO: more tests. 
    
    There is a bit more that could be done, but this is getting large already.
    
    Fixes #1197

--- mathics/eval/distance.py
+++ mathics/eval/distance.py
@@ -2,7 +2,9 @@
 Distance-related evaluation functions and exception classes
 """
 
-from mathics.core.atoms import Integer, Real
+from mathics.core.atoms import Complex, Integer, Integer0, Real
+from mathics.core.convert.python import from_python
+from mathics.core.convert.sympy import from_sympy, to_sympy_matrix
 
 
 class IllegalDataPoint(Exception):
@@ -31,6 +33,38 @@ def dist_repr(p) -> tuple:
         else:
             dist_p = repr_p = p.elements
     return dist_p, repr_p
+
+
+def eval_CosineDistance(u, v):
+    "CosineDistance[u_, v_]"
+
+    # We follow pretty much what is done in Symja in ClusteringFunctions.java for this:
+    # https://github.com/axkr/symja_android_library/blob/master/symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/ClusteringFunctions.java#L377-L386
+    # Some extensions for degenerate cases have been added.
+
+    # Handle some degenerate cases
+    if isinstance(u, (Complex, Integer, Real)) and isinstance(
+        v, (Complex, Integer, Real)
+    ):
+        u_val = u.to_python()
+        v_val = v.to_python()
+        distance = 1 - u_val * v_val.conjugate() / (abs(u_val) * abs(v_val))
+        return from_python(distance)
+
+    sym_u = to_sympy_matrix(u)
+    if sym_u is None:
+        return
+    sym_v = to_sympy_matrix(v)
+    if sym_v is None:
+        return
+    u_norm = sym_u.norm()
+    if u_norm == 0:
+        return Integer0
+    v_norm = sym_v.norm()
+    if v_norm == 0:
+        return Integer0
+
+    return from_sympy(1 - sym_u.dot(sym_v.conjugate()) / (u_norm * v_norm))
 
 
 def to_real_distance(d):
