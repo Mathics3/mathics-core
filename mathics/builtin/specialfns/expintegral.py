39729b1b04250ad56224764edda35dd7509f50b0
rocky <rb@dustyfeet.com>
2025-02-06 19:35:19 -0500

    Add more DMLF links and example plots

--- mathics/builtin/specialfns/expintegral.py
+++ mathics/builtin/specialfns/expintegral.py
@@ -2,6 +2,10 @@
 
 """
 Exponential Integral and Special Functions
+
+See also <url>
+:Chapters 4.2-4.13 Logarithm, Exponential, Powers in the Digital Libary of Mathematical Functions:
+https://dlmf.nist.gov/4#PT2</url>.
 """
 
 
