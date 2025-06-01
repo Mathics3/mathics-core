a16d518d61d034f3df0610db89778f1d760ce04c
rocky <rb@dustyfeet.com>
2025-06-01 09:52:14 -0400

    Add a couple syntax tests

--- test/core/parser/test_util.py
+++ test/core/parser/test_util.py
@@ -5,6 +5,7 @@ from mathics_scanner import (
     InvalidSyntaxError,
     MultiLineFeeder,
     SingleLineFeeder,
+    SyntaxError,
 )
 
 from mathics.core.definitions import Definitions
@@ -37,6 +38,9 @@ class UtilTests(unittest.TestCase):
     def invalid_error(self, string):
         self.assertRaises(InvalidSyntaxError, self.parse, string)
 
+    def syntax_error(self, string):
+        self.assertRaises(SyntaxError, self.parse, string)
+
 
 class SingleLineParserTests(UtilTests):
     def parse(self, code):
@@ -52,6 +56,7 @@ class SingleLineParserTests(UtilTests):
 
     def test_trailing_backslash(self):
         self.incomplete_error("x \\")
+        self.syntax_error("X\\n\\t")
 
         ## TODO see what this should do and why
         ## self.check("x \\\ny", "Times[x, y]")
@@ -66,6 +71,7 @@ class MultiLineParserTests(UtilTests):
 
     def test_trailing_backslash(self):
         self.incomplete_error("x \\")
+        self.syntax_error("X\\n\\t")
 
         ## TODO see what this should do and why
         ## self.check("x \\\ny", "Times[x, y]")
