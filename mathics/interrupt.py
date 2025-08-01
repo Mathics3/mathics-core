5994cfaf65b29e7f25bc28a10d57ae83c7f49d46
rocky <rb@dustyfeet.com>
2025-08-01 11:43:11 -0400

    Finish interrupt handler 1st cut...
    * printing new uses shell print routine
    * in "inspect" command, we get the prompting "In[]" right not
    * in "show" we show both the expression and the Builtin signature or
      __doc__ string
    * disable "trace" command for now. That will be another PR

--- mathics/interrupt.py
+++ mathics/interrupt.py
@@ -4,16 +4,16 @@ Default Mathics3 Interrupt routines.
 Note: other environments may build on or use other interrupt handlers
 """
 
-import inspect
 import signal
 import subprocess
 import sys
-from typing import Optional
+from types import FrameType
+from typing import Callable, Optional
 
 from mathics import settings
 from mathics.core.evaluation import Evaluation
 from mathics.core.interrupt import AbortInterrupt, ReturnInterrupt, TimeoutInterrupt
-from mathics.eval.stackframe import find_Mathics3_evaluation_method
+from mathics.eval.stackframe import find_Mathics3_evaluation_method, get_eval_Expression
 
 
 # See also __main__'s interactive_eval_loop
@@ -31,6 +31,12 @@ def inspect_eval_loop(evaluation: Evaluation):
     previous_recursion_depth = evaluation.recursion_depth
     while True:
         try:
+            # Reset line number within an In[] line number.
+            # Note: this is not setting as, say, In[5]
+            # to back to In[1], but instead it sets the line number position *within*
+            # In[5]. The user input for "In[5]" might have several continuation lines.
+            shell.lineno = 0
+
             query, source_code = evaluation.parse_feeder_returning_code(shell)
             # show_echo(source_code, evaluation)
             if len(source_code) and source_code[0] == "!" and shell is not None:
@@ -65,7 +71,9 @@ def inspect_eval_loop(evaluation: Evaluation):
                 shell.is_inside_interrupt = was_inside_interrupt
 
 
-def Mathics3_interrupt_handler(evaluation: Optional[Evaluation]):
+def Mathics3_interrupt_handler(
+    evaluation: Optional[Evaluation], interrupted_frame: FrameType, print_fn: Callable
+):
     try:
         import readline  # noqa
     except ImportError:
@@ -75,16 +83,16 @@ def Mathics3_interrupt_handler(evaluation: Optional[Evaluation]):
         try:
             user_input = input("interrupt> ").strip()
             if user_input in ("a", "abort"):
-                print("aborting")
+                print_fn("aborting")
                 raise AbortInterrupt
             elif user_input in ("continue", "c"):
-                print("continuing")
+                print_fn("continuing")
                 break
             elif user_input in ("exit", "quit"):
-                print("Mathics3 exited because of an interrupt.")
+                print_fn("Mathics3 exited because of an interrupt.")
                 sys.exit(3)
             elif user_input in ("inspect", "i"):
-                print("inspecting")
+                print_fn("inspecting")
                 if evaluation is not None:
                     evaluation.message("Interrupt", "dgbgn")
                     inspect_eval_loop(evaluation)
@@ -92,33 +100,33 @@ def Mathics3_interrupt_handler(evaluation: Optional[Evaluation]):
             elif user_input in ("show", "s"):
                 # In some cases we can better, by going back to the caller
                 # and reconstructing the actual call with arguments.
-                eval_frame = find_Mathics3_evaluation_method(inspect.currentframe())
+                eval_frame = find_Mathics3_evaluation_method(interrupted_frame)
                 if eval_frame is None:
                     continue
                 eval_method_name = eval_frame.f_code.co_name
                 eval_method = getattr(eval_frame.f_locals.get("self"), eval_method_name)
                 if eval_method:
-                    print(eval_method.__doc__)
-                else:
-                    print(eval_frame)
+                    print_fn(eval_method.__doc__)
+                eval_expression = get_eval_Expression()
+                if eval_expression is not None:
+                    print_fn(str(eval_expression))
                 break
             elif user_input in ("trace", "t"):
-                print("tracing")
+                print_fn("tracing")
             else:
-                print(
+                print_fn(
                     """Your options are:
 	abort (or a) to abort current calculation
 	continue (or c) to continue
 	exit (or quit) to exit Mathics3
 	inspect (or i) to enter an interactive dialog
-	show (or s) to show current operation (and then continue"
-	trace (or t) to show all operations"
+	show (or s) to show current operation (and then continue)
 """
                 )
         except KeyboardInterrupt:
-            print("\nKeyboardInterrupt")
+            print_fn("\nKeyboardInterrupt")
         except EOFError:
-            print()
+            print_fn("")
             break
         except TimeoutInterrupt:
             # There might have been a Pause[] set before we entered
@@ -142,22 +150,23 @@ def Mathics3_interrupt_handler(evaluation: Optional[Evaluation]):
             pass
 
 
-def Mathics3_basic_signal_handler(sig, current_frame):
+def Mathics3_basic_signal_handler(sig, interrupted_frame):
     """
     Custom signal handler for SIGINT (Ctrl+C).
     """
     evaluation: Optional[Evaluation] = None
     # Find an evaluation object to pass to the Mathics3 interrupt handler
-    while current_frame is not None:
+    while interrupted_frame is not None:
         if (
-            evaluation := current_frame.f_locals.get("evaluation")
+            evaluation := interrupted_frame.f_locals.get("evaluation")
         ) is not None and isinstance(evaluation, Evaluation):
             break
-        current_frame = current_frame.f_back
-    print()
-    if current_frame is None:
+        interrupted_frame = interrupted_frame.f_back
+    print_fn = evaluation.print_out if evaluation is not None else print
+    print_fn("")
+    if interrupted_frame is None:
         print("Unable to find Evaluation frame to start on")
-    Mathics3_interrupt_handler(evaluation)
+    Mathics3_interrupt_handler(evaluation, interrupted_frame, print_fn)
 
 
 def setup_signal_handler():
