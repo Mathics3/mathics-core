9d9f040d396fd0e4b7c7b304668b218d692fc43c
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2025-08-01 00:15:00 -0300

    adding regular sorting test

--- test/core/test_expression.py
+++ test/core/test_expression.py
@@ -9,16 +9,32 @@ from mathics.core.expression import Expression
 from mathics.core.symbols import Symbol, SymbolPlus, SymbolTimes
 
 
-# FIXME: come up with an example that doesn't require skimage.
-@pytest.mark.skipif(
-    not check_requires_list(["skimage"]),
-    reason="Right now need scikit-image for this to work",
-)
 def test_canonical_sort():
     check_evaluation(
-        r'Sort[{Import["ExampleData/Einstein.jpg"], 5}]',
-        r'{5, Import["ExampleData/Einstein.jpg"]}',
+        """
+    Sort[{
+    "a","b", 1, 
+    ByteArray[{1,2,4,1}], 
+    2, 1.2, I, 2I-3, A, 
+    a+b, a*b, a+1, a*2, b^3, 2/3, 
+    A[x], F[2], F[x], F[x_], F[x___], F[x,t], F[x__], 
+    Condition[A,b>2], Pattern[expr, A]
+    }]
+    """,
+        """{ -3 + 2*I, I, 2 / 3, 1, 1.2, 2, 
+         "a", "b", A, 2*a, a*b, b^3, 
+          A[x], F[2], F[x], F[x_], F[x___], F[x__], F[x, t], 
+          ByteArray["AQIEAQ=="], A /; b > 2, 
+          expr:A, 1 + a, a + b}""",
     )
+    # The right canonical order should be, according to WMA:
+    #     -3 + 2*I, I, 2/3, 1, 1.2, 2,
+    #     "a", "b", 2*a,
+    #      1 + a, A, a*b, b^3, a + b,
+    #     A[x], A /; b > 2,
+    #     F[2], F[x], F[x_], F[x___], F[x__], F[x, t],
+    #     ByteArray["AQIEAQ=="], expr:A
+
     check_evaluation(
         r"Sort[Table[IntegerDigits[2^n], {n, 10}]]",
         r"{{2}, {4}, {8}, {1, 6}, {3, 2}, {6, 4}, {1, 2, 8}, {2, 5, 6}, {5, 1, 2}, {1, 0, 2, 4}}",
@@ -26,6 +42,18 @@ def test_canonical_sort():
     check_evaluation(
         r"SortBy[Table[IntegerDigits[2^n], {n, 10}], First]",
         r"{{1, 6}, {1, 2, 8}, {1, 0, 2, 4}, {2}, {2, 5, 6}, {3, 2}, {4}, {5, 1, 2}, {6, 4}, {8}}",
+    )
+
+
+# FIXME: come up with an example that doesn't require skimage.
+@pytest.mark.skipif(
+    not check_requires_list(["skimage"]),
+    reason="Right now need scikit-image for this to work",
+)
+def test_canonical_sort_images():
+    check_evaluation(
+        r'Sort[{Import["ExampleData/Einstein.jpg"], 5}]',
+        r'{5, Import["ExampleData/Einstein.jpg"]}',
     )
 
 
