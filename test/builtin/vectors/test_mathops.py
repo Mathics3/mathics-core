4ee812a6707bb401004c35cccad63a0a60e985a3
R. Bernstein <rocky@users.noreply.github.com>
2025-08-21 06:48:14 -0400

    Add arg checking to Curl... (#1472)
    We use Curl as a developer doc example, so we should have this more correct.

--- test/builtin/vectors/test_mathops.py
+++ test/builtin/vectors/test_mathops.py
@@ -2,10 +2,12 @@
 """
 Unit tests for mathics.builtin.vectors.math_ops
 
-In particular, Norm[].
+In particular, Curl[] and Norm[].
 """
 
 from test.helper import check_evaluation
+
+import pytest
 
 
 def test_norm():
@@ -48,3 +50,31 @@ def test_norm():
         ),
     ):
         check_evaluation(str_expr=str_expr, str_expected=str_expected)
+
+
+@pytest.mark.parametrize(
+    ("str_expr", "msgs", "assert_fail_msg"),
+    [
+        (
+            "Curl[a]",
+            ["Curl called with 1 argument; 2 or 3 arguments are expected."],
+            "Curl with one argument",
+        ),
+        (
+            "Curl[a, b, c, d]",
+            ["Curl called with 4 arguments; 2 or 3 arguments are expected."],
+            "Curl with more than three arguments",
+        ),
+    ],
+)
+def test_wrong_number_of_arguments(str_expr, msgs, assert_fail_msg):
+    """ """
+    check_evaluation(
+        str_expr,
+        str_expr,
+        to_string_expr=True,
+        to_string_expected=True,
+        hold_expected=True,
+        failure_message=assert_fail_msg,
+        expected_messages=msgs,
+    )
