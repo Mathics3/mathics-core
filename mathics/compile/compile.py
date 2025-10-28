2124e817e8286d77d380b55fc30757d0b52db63e
rocky <rb@dustyfeet.com>
2025-10-28 15:15:25 -0400

    Remove LLVM initialization.

--- mathics/compile/compile.py
+++ mathics/compile/compile.py
@@ -6,7 +6,7 @@ from mathics.compile.ir import IRGenerator
 from mathics.compile.utils import llvm_to_ctype
 
 # setup llvm for code generation
-llvm.initialize()
+# llvm.initialize()
 llvm.initialize_native_target()
 llvm.initialize_native_asmprinter()  # yes, even this one
 
