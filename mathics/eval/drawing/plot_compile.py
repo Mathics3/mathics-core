195c601ee76dca9f6c229659d3a54e102ff0bd57
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-11-15 06:55:48 -0500

    WIP

--- mathics/eval/drawing/plot_compile.py
+++ mathics/eval/drawing/plot_compile.py
@@ -13,29 +13,28 @@ Maybe eventually consider to move elsewhere if it seems to be more
 widely useful.
 """
 
-
 import scipy
 import sympy
 
 from mathics.core.symbols import strip_context
 from mathics.core.util import print_expression_tree, print_sympy_tree
 
+class AdditionalMappings:
 
-def hyppfq(p, q, x):
-    if len(p) == 1 and len(q) == 1:
-        return scipy.special.hyp1f1(p[0], q[0], x)
-    else:
-        raise Exception(f"can't handle hyppfq({p}, {q}, x)")
+    def hyppfq(p, q, x):
+        if len(p) == 1 and len(q) == 1:
+            return scipy.special.hyp1f1(p[0], q[0], x)
+        else:
+            raise Exception(f"can't handle hyppfq({p}, {q}, x)")
 
+#mappings = [dict(AdditionalMappings.__dict__), "numpy"]
 
-# mappings for sympy functions that aren't handled by
-# the default mapping to numpy
-additional_mappings = {"hyper": hyppfq}
-
+class CompileError(Exception):
+    pass
 
 def compile(evaluation, function, names):
-    print("=== compiling expr")
-    print_expression_tree(function)
+    #print("=== compiling expr")
+    #print_expression_tree(function)
 
     # Obtain sympy expr and strip context from free symbols.
     sympy_expr = function.to_sympy()
@@ -44,14 +43,17 @@ def compile(evaluation, function, names):
     }
     sympy_expr = sympy_expr.subs(subs)
 
-    print("=== compiled sympy", type(sympy_expr))
-    print_sympy_tree(sympy_expr)
+    #print("=== compiled sympy", type(sympy_expr))
+    #print_sympy_tree(sympy_expr)
 
     # Ask sympy to generate a function that will evaluate the expr.
     # Use numpy to do the evaluation so that operations are vectorized.
     # Augment the default numpy mappings with some additional ones not handled by default.
-    compiled_function = sympy.lambdify(
-        sympy.symbols(names), sympy_expr, [additional_mappings, "numpy"]
-    )
+    try:
+        symbols = sympy.symbols(names)
+        #compiled_function = sympy.lambdify(symbols, sympy_expr, mappings)
+        compiled_function = sympy.lambdify(symbols, sympy_expr, modules=["numpy", "scipy"])
+    except Exception as oops:
+        raise CompileError(f"error compiling sympy expr {sympy_expr}: {oops}")
 
     return compiled_function
