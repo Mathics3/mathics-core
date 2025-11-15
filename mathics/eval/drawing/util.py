d0677dba6f6a46b484d610bdf9c17d39b75902a8
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-11-15 13:46:52 -0500

    Small refactoring
    Generate Graphics elements as-you-go instead of at generate time.

--- mathics/eval/drawing/util.py
+++ mathics/eval/drawing/util.py
@@ -12,6 +12,7 @@ from mathics.core.symbols import Symbol
 from mathics.core.systemsymbols import (
     SymbolGraphics,
     SymbolGraphics3D,
+    SymbolGraphicsComplex,
     SymbolLine,
     SymbolPolygon,
     SymbolRule,
@@ -27,84 +28,59 @@ class GraphicsGenerator:
     Support for generating Graphics and Graphics3D expressions
     """
 
-    # TODO: more precise types
-    # TODO: consider pre-zipping so only one for polys and one for lines
-    # TODO: consider whether we need to store these or can we just generate as we go?
-    poly_xyzs: list
-    poly_xyzs_colors: list
-    line_xyzs: list
-    line_xyzs_colors: list
-    complexes: list
+    # TODO: more precise typing?
+    graphics: list
 
     # 2 or 3
     dim: int
 
     def __init__(self, dim: int):
         self.dim = dim
-        self.poly_xyzs = []
-        self.poly_xyzs_colors = []
-        self.line_xyzs = []
-        self.line_xyzs_colors = []
-        self.complexes = []
+        self.graphics = []
 
-    # TODO: is this correct if some polys have colors and some don't?
+    # add polygons and lines, optionally with vertex colors
+    def add_thing(self, thing_symbol, things, colors):
+        arg = tuple(to_mathics_list(*thing) for thing in things)
+        arg = ListExpression(*arg) if len(arg) > 1 else arg[0]
+        if colors:
+            color_arg = tuple(to_mathics_list(*color) for color in colors)
+            color_arg = (
+                ListExpression(*color_arg)
+                if len(color_arg) > 1
+                else color_arg[0]
+            )
+            color_rule = Expression(SymbolRule, SymbolVertexColors, color_arg)
+            self.graphics.append(Expression(thing_symbol, arg, color_rule))
+        else:
+            self.graphics.append(Expression(thing_symbol, arg))
+
     def add_polyxyzs(self, poly_xyzs, colors=None):
         """Add polygons specified by explicit xy[z] coordinates"""
-        self.poly_xyzs.append(poly_xyzs)
-        if colors:
-            self.poly_xyzs_colors.append(colors)
+        self.add_thing(SymbolPolygon, poly_xyzs, colors)
 
     def add_linexyzs(self, line_xyzs, colors=None):
         """Add lines specified by explicit xy[z] coordinates"""
-        self.line_xyzs.append(line_xyzs)
-        if colors:
-            self.line_xyzs_colors.append(colors)
+        self.add_thing(SymbolLine, line_xyzs, colors)
 
-    def add_complex(self, xyzs, lines, polys):
-        self.complexes.append((xyzs, lines, polys))
+    # TODO: color
+    def add_complex(self, xyzs, lines=None, polys=None):
+        complex = [NumericArray(xyzs)]
+        if polys is not None:
+            polys_expr = Expression(SymbolPolygon, NumericArray(polys))            
+            complex.append(polys_expr)
+        if lines is not None:
+            polys_expr = Expression(SymbolLines, NumericArray(lines))
+            complex.append(lines_expr)
+        gc_expr = Expression(SymbolGraphicsComplex, *complex)
+        self.graphics.append(gc_expr)
 
     def generate(self, options):
         """
         Generates Graphics[3D] expression from supplied lines, polygons (etc.)
         """
-
-        # holds the elements of the final Graphics[3D] expr
-        graphics = []
-
-        # add polygons and lines, optionally with vertex colors
-        def add_thing(thing_symbol, thingss, colorss):
-            for things, colors in itertools.zip_longest(thingss, colorss):
-                arg = tuple(to_mathics_list(*thing) for thing in things)
-                arg = ListExpression(*arg) if len(arg) > 1 else arg[0]
-                if colors:
-                    color_arg = tuple(to_mathics_list(*color) for color in colors)
-                    color_arg = (
-                        ListExpression(*color_arg)
-                        if len(color_arg) > 1
-                        else color_arg[0]
-                    )
-                    color_rule = Expression(SymbolRule, SymbolVertexColors, color_arg)
-                    graphics.append(Expression(thing_symbol, arg, color_rule))
-                else:
-                    graphics.append(Expression(thing_symbol, arg))
-
-        add_thing(SymbolPolygon, self.poly_xyzs, self.poly_xyzs_colors)
-        add_thing(SymbolLine, self.line_xyzs, self.line_xyzs_colors)
-
-        # add complexes
-        for xyzs, lines, polys in self.complexes:
-            # TODO: lines
-            xyzs_expr = NumericArray(xyzs)
-            polys_expr = Expression(SymbolPolygon, NumericArray(polys))
-            # TODO: move to systemsymbols
-            SymbolGraphicsComplex = Symbol("System`GraphicsComplex")
-            gc_expr = Expression(SymbolGraphicsComplex, xyzs_expr, polys_expr)
-            graphics.append(gc_expr)
-
-        # generate Graphics[3D] expression
         graphics_expr = Expression(
             SymbolGraphics3D if self.dim == 3 else SymbolGraphics,
-            ListExpression(*graphics),
+            ListExpression(*self.graphics),
             *options,
         )
 
