cb156ffa56041ab266122f89605eb93d3a78fa0a
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2026-01-17 13:47:42 -0300

    Improve SeriesData format function (#1627)
    This PR reformulates the formatting of `SeriesData` expressions, coming
    from `Series` and related functions. Instead of working at the level of
    boxes, this implementation works at the level of `FormatValues`, in a
    way that it can work with the new implementation of `OutputForm`.

--- mathics/builtin/list/constructing.py
+++ mathics/builtin/list/constructing.py
@@ -188,7 +188,8 @@ class Normal(Builtin):
     >> Normal[Pi]
      = Pi
     >> Series[Exp[x], {x, 0, 5}]
-     = 1 + x + 1 / 2 x ^ 2 + 1 / 6 x ^ 3 + 1 / 24 x ^ 4 + 1 / 120 x ^ 5 + O[x] ^ 6
+     = 1 + x + x ^ 2 / 2 + x ^ 3 / 6 + x ^ 4 / 24 + x ^ 5 / 120 + O[x] ^ 6
+
     >> Normal[%]
      = 1 + x + x ^ 2 / 2 + x ^ 3 / 6 + x ^ 4 / 24 + x ^ 5 / 120
     """
