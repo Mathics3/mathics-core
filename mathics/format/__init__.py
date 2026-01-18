8e3f0e165e05d05984e5e6995a4708b312d9fa73
rocky <rb@dustyfeet.com>
2026-01-18 13:15:34 -0500

    tweak format docstring

--- mathics/format/__init__.py
+++ mathics/format/__init__.py
@@ -1,18 +1,16 @@
 """
-This module contains the structures and functions used to export
-Mathics3 base elements into a formatted representation as plane text,
-LaTeX, MathML and other markdown formatted text representations.
-This process involves:
+This module contains the structures and functions used to turn Mathics3 expressions into
+renderable output.
 
-* Apply FormatValues rules
-* Apply MakeBoxes rules to convert Mathics3 expressions into 
-  different kind of Box Expressions
-* Process Box expressions to convert them into a text strings following a certain format convension/encoding.
+The three-step process is:
 
-For example, to convert the expression `Plus[a, Times[-1, b], c]` into a text representation, `a - b + c`
-1. Applying  format rules, the expression is reformatted as `Infix[{a, b, c},{"-", "+"}, 310, Left]` 
-2. Apply MakeBoxes rules to build, for example, its `StandardForm` Box expression`RowBox[{"a", "-", "b", "+", "c"}]`
-3. Finally, the Box expression is converted to the text string
- "a - b + c" calling functions from the module `mathics.format.export.text`
+1. Apply any rewrite rules that are set by default or specified by rules explicitly given using builtin function `Format`.
+2. Box the these resulting expression giving boxed expressions.
+3. Render the boxed expressions to strings
 
+For example, to format the expression `Plus[a, Times[-1, b], c]` in `StandardForm`:
+
+1. Format rules from `mathics.format.form_rule` rewrite as  `Infix[{a, b, c}, {"-", "+"}, 310, Left]`.
+2. Boxing rules based on a particular Form are run. For `StandardForm`, the Box expression of the above is `RowBox[{"a", "-", "b", "+", "c"}]`
+3. Box expressions using functions from the module `mathics.format.render.text` render this as string: `a - b + c`
 """
