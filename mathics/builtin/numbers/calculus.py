0109171d4231bb879910f0ec9f6b4ce59ef4f50c
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2026-01-18 13:13:58 -0300

    moving implementation of format methods to mathics.format.formvalues

--- mathics/builtin/numbers/calculus.py
+++ mathics/builtin/numbers/calculus.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """
 Calculus
 
@@ -97,6 +96,7 @@ from mathics.eval.numbers.calculus.series import (
     series_plus_series,
     series_times_series,
 )
+from mathics.format.formvalues.calculus import format_series
 from mathics.format.makeboxes import format_element
 
 # These should be used in lower-level formatting
@@ -2244,61 +2244,7 @@ class SeriesData(Builtin):
             x_, x0_, data_List, nmin_Integer, nmax_Integer, den_Integer
         ]
         """
-        if den.value != 1:
-            powers = [Rational(i, den) for i in range(nmin.value, nmax.value + 1)]
-        else:
-            powers = [Integer(i) for i in range(nmin.value, nmax.value + 1)]
-
-        terms = []
-        base = x
-        if not x0.is_zero:
-            base = base + (-x0).evaluate(evaluation)
-
-        factors = data.elements
-        if len(factors) >= len(powers):
-            factors = factors[: len(powers)]
-
-        for idx, prefactor in enumerate(factors):
-            if prefactor is Integer0:
-                continue
-            power = powers[idx]
-            if power is Integer0:
-                terms.append(prefactor)
-                continue
-            if power is Integer1:
-                term = base
-            else:
-                term = base**power
-
-            if prefactor is Integer1:
-                terms.append(term)
-                continue
-            if isinstance(prefactor, Rational):
-                num_value, den_value = prefactor.value.as_numer_denom()
-                negative = False
-                if num_value < 0:
-                    num_value, negative = -num_value, True
-                if num_value == 1:
-                    term = Expression(SymbolDivide, term, Integer(den_value))
-                else:
-                    term = Expression(
-                        SymbolDivide, Integer(num_value) * term, Integer(den_value)
-                    )
-                if negative:
-                    term = IntegerM1 * term
-            else:
-                term = prefactor * term
-            terms.append(term)
-
-        regular = Expression(SymbolHoldForm, Expression(SymbolPlus, *terms))
-        last = Expression(SymbolHoldForm, Expression(SymbolO, base) ** (powers[-1]))
-        return Expression(
-            SymbolInfix,
-            ListExpression(regular, last),
-            String("+"),
-            Integer310,
-            SymbolLeft,
-        )
+        return format_series(x, x0, data, nmin, nmax, den, evaluation)
 
 
 class Solve(Builtin):
