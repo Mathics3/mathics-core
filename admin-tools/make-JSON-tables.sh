7d33b83a3401a4e85cd9928bd04207390deb9222
R. Bernstein <rocky@users.noreply.github.com>
2026-02-03 05:53:58 -0500

    Don't hardcode unicode-symbols (#1673)
    Don't hardcode Unicode symbols. Also, `op-tables.json` is renamed `character-tables.json`, since that's what it is.

--- admin-tools/make-JSON-tables.sh
+++ admin-tools/make-JSON-tables.sh
@@ -5,16 +5,7 @@ mydir=$(dirname $bs)
 PYTHON=${PYTHON:-python}
 
 cd $mydir/../mathics/data
-mathics3-generate-json-table \
-    --field=latex-named-characters \
-    --field=unicode-to-latex \
-    --field=ascii-operator-to-symbol \
-    --field=ascii-operator-to-unicode \
-    --field=ascii-operator-to-wl-unicode \
-    --field=operator-to-ascii \
-    --field=operator-to-amslatex \
-    --field=operator-to-unicode \
-    -o op-tables.json
+mathics3-generate-json-table -o character-tables.json
 mathics3-generate-operator-json-table -o operator-tables.json
 # tokenizer looks for the table in the default place...
 mathics3-generate-operator-json-table
