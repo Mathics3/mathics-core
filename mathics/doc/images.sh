e9907781cb78872bd899e6628d4a8b3dd09821b2
R. Bernstein <rocky@users.noreply.github.com>
2026-03-27 15:23:36 -0400

    Go over Mathics3 logo (#1760)

--- mathics/doc/images.sh
+++ mathics/doc/images.sh
@@ -8,24 +8,26 @@ bs=${BASH_SOURCE[0]}
 mydir=$(dirname $bs)
 cd $mydir
 mydir=$(pwd)
+DOC_DIR=$(pwd)
 
 if [[ -n $DOCTEST_LATEX_DATA_PCL ]]; then
     LATEX_DIR=$(basename $DOCTEST_LATEX_DATA_PCL)
 else
     LATEX_DIR=${mydir}/latex
 fi
-IMAGE_DIR=${LATEX_DIR}/images
+LATEX_IMAGE_DIR=${LATEX_DIR}/images
+DOC_IMAGE_DIR=${DOC_DIR}/documentation/images
 
-if [[ ! -d "$IMAGE_DIR" ]] ; then
-    mkdir -p $IMAGE_DIR
+if [[ ! -d "$DOC_IMAGE_DIR" ]] ; then
+    mkdir -p $DOC_IMAGE_DIR
 fi
 
 for filename in $(find documentation/images/ -name "*.eps"); do
-	pdf="${LATEX_DIR}/$(basename "$filename" .eps).pdf"
+	pdf="${DOC_IMAGE_DIR}/$(basename "$filename" .eps).pdf"
 	epstopdf "$filename"
-	mv "$pdf" $IMAGE_DIR
+	mv "$pdf" $LATEX_IMAGE_DIR
 done
 
-for filename in ${mydir}/images/logo-{heptatom,text-nodrop}.svg; do
+for filename in ${mydir}/images/logo-{heptatom,heptatom-Mathics3,Mathics3-nodrop}.svg; do
     inkscape $filename --export-filename="latex/$(basename "$filename" .svg).pdf" --batch-process
 done
--- mathics/doc/images/README.md
+++ mathics/doc/images/README.md
@@ -1,1 +1,10 @@
-The SVG files here, currently logos, are turned into PDFs which are used in the Mathics book (tutorial + reference manual).
+The SVG files here, currently logos, are turned into PDFs which are used in the Mathics3 book (tutorial + reference manual).
+
+The logo also appears in Mathics3-django and possibly other places outside of this repository.
+
+About the Logo...
+
+It uses the Times New Roman font. The "3" is italicized.
+There is a reflected image that is slanted about 30 per cent and has a gradient on the reflection.
+
+It was createed in inkscape, and can probably be edited that way.
