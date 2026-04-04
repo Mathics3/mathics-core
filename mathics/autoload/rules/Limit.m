1ff36f8ec434a70c8514c223f6d6984806f4cd2e
rocky <rb@dustyfeet.com>
2026-04-04 19:01:57 -0400

    Limit-inspired changes
    Improve argument checking in builtins with options to emit "nonopt" tags
    when appropriate
    
    Improve from_sympy so it understands SymPy reporting Indeterminate
    
    Go over Limit docs, and autoload rules
    
    Expand limit tests

--- mathics/autoload/rules/Limit.m
+++ mathics/autoload/rules/Limit.m
@@ -1,3 +1,4 @@
+(* -*- wolfram -*- *)
 (* Additions to mathics.builtin.numbers.trig that are either wrong
    or not covered by SymPy.
 
@@ -7,8 +8,16 @@
 
 Begin["System`Limit`private`"]
 Unprotect[Limit];
+
+(* We have the following Indeterminate rules on Limit to prevent them
+   from coming out Infinity. Other cases like:
+     Limit[Tan[x], x->Infinity] or
+     Limit[Cot[x], x->Infinity]
+   SymPy matches WMA.
+ *)
 Limit[Tan[x_], x_Symbol->Pi/2] = Indeterminate;
 Limit[Cot[x_], x_Symbol->0] = Indeterminate;
+
 Limit[x_ Sqrt[2 Pi]^(x_^-1) (Sin[x_]/(x_!))^(x_^-1), x_Symbol->Infinity] = E;
 Protect[Limit];
 End[]
