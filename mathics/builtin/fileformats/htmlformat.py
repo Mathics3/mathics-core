ba5d7905b03e09c206b3a77775366bd098723271
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2026-04-04 13:02:00 -0300

    fix wrong character

--- mathics/builtin/fileformats/htmlformat.py
+++ mathics/builtin/fileformats/htmlformat.py
@@ -280,7 +280,7 @@ class DataImport(_DataImport):
     </dl>
 
     >> Import["ExampleData/PrimeMeridian.html", "Data"][[1, 1, 2, 3]]
-     = {Washington, D.C., 77º03′56.07″ W (1897) or 77º04′02.24″ W (NAD 27) or 77º04′01.16″ W (NAD 83), New Naval Observatory meridian}
+     = {Washington, D.C., 77°03′56.07″ W (1897) or 77°04′02.24″ W (NAD 27) or 77°04′01.16″ W (NAD 83), New Naval Observatory meridian}
 
     #> Length[Import["ExampleData/PrimeMeridian.html", "Data"]]
      = 3
