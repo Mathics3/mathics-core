19ded2785dff25ceb9545e177fcb8d66127dac75
rocky <rb@dustyfeet.com>
2026-04-12 21:31:22 -0400

    Partial try to get doc building
    sed-hack.hl Replace in hacky way all ==
    mathics3-test.tex: add harness for test checking
    mathics3.tex: remove a LaTeX warning
    Makefile: mathics -> mathics3

--- mathics/builtin/files_io/files.py
+++ mathics/builtin/files_io/files.py
@@ -357,7 +357,7 @@ class Get(PrefixOperator):
       <dt>'Path'->$dir$
       <dd>Set the search path to the single directory $dir$ in the 'Get'.
       <dt>'Path'->{"$dir_1$", "$dir_2$", ...}
-      <dd>Set the search path, '$PATH' to the list of directories.
+      <dd>Set the search path, '\$PATH' to the list of directories.
       <dt>'CharacterEncoding'->"{$name}"
       <dd>Set the file input encoding to $name$.
     </dl>
