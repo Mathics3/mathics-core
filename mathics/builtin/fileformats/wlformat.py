c6328f11a6ba26a45dde87a8ee4e0afb3ffd9169
rocky <rb@dustyfeet.com>
2026-07-28 12:30:44 -0400

    Promote a 'WLFormat' pytest to doctests and demonstrate explanation of 'WLFormat' vs. 'Import' and 'Get'

--- mathics/builtin/fileformats/wlformat.py
+++ mathics/builtin/fileformats/wlformat.py
@@ -18,6 +18,30 @@ class ImportWL(Builtin):
       <dd>Read $path$ as a Wolfram package and 'Get' it.
     </dl>
 
+    This command is hooked into the 'Import' command via \
+    'SystemFiles/Formats/WL/Import.wl'.
+
+    When running this command directly, a list of rules is returned:
+
+    >> WLDump`ImportWL["ExampleData/HelloWorld.wl"]
+     = {Get -> Hello, World!, Script -> Hello, World!}
+
+    The left-hand side is a element name that can be provided to 'Import', \
+    and the right-hand side is the value associated with that element. Here it \
+    represents the last evaluation value when evaluating the contents of the file.
+
+    When an 'Import' is done and the file mentioned is a Mathics3 or a \
+    Wolfram-Language file, this command is run underneath selecting value of the \
+    "Get" element:
+
+    >> Import["ExampleData/HelloWorld.wl"]
+     = Hello, World!
+
+    All of this is the same as just running a simple 'Get':
+    >> << "ExampleData/HelloWorld.wl"
+     = Hello, World!
+
+    In sum, the built-in provides the glue to the 'Import' accees mechanism for 'Get'.
     """
 
     context = "WLDump`"
