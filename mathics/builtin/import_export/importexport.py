1c9b71ae7df4239540eb44a08cd7f707831826a0
rocky <rb@dustyfeet.com>
2026-09-21 10:03:24 -0400

    Another round of URL/LaTeX link corrections

--- mathics/builtin/import_export/importexport.py
+++ mathics/builtin/import_export/importexport.py
@@ -2,33 +2,6 @@
 
 r"""
 Import and Export Functions and Variables
-
-Many kinds data formats can be read into or written from \Mathics3.
-
-In contrast to reading or writing a file, <i>importing</i> and <i>exporting</i> imply some sort of \
-data restructuring into \Mathics3 and structuring into a filesystem that is not \
-just a stream of bytes, but instead also contains additional metadata and requires data reorganization \
-when stored in a filesystem.
-
-See <url>
-:Import/Export File Formats:
-/doc/reference-of-built-in-symbols/fileformats/</url> for documentation \
-on the specific kinds of File Formats \Mathics3 supports.
-
-
-Variable <url>
-:\$ExportFormats:
-/doc/reference-of-built-in-symbols/inputoutput-files-and-filesystem/importing-and-exporting/\$exportformats</url> \
-contains a list of file formats that are supported by <url>
-:Export:
-/doc/reference-of-built-in-symbols/inputoutput-files-and-filesystem/importing-and-exporting/export</url>, \
-while <url>
-:\$ImportFormats:
-/doc/reference-of-built-in-symbols/inputoutput-files-and-filesystem/importing-and-exporting/\$importformats</url> \
-does the corresponding thing for <url>
-:Import:
-/doc/reference-of-built-in-symbols/inputoutput-files-and-filesystem/importing-and-exporting/import</url>.
-
 """
 
 import base64
